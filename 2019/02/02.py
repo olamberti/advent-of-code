72d05dec1a899cb8c080460c70779cfab62f3ccb
Oliver Lamberti <lamberti.oliver@gmail.com>
2023-05-24 21:54:18 +0200

    updated with IntCode class

--- 2019/02/02.py
+++ 2019/02/02.py
@@ -2,20 +2,34 @@ from copy import deepcopy as dp
 memory = [int(x) for x in open('02.txt').read().split(',')]
 memory_ = dp(memory)
 
-def run(mem):
-  p = 0
-  while mem[p] != 99:
-    inst = mem[p]
-    if inst == 1 or inst == 2:
-      a, b, t = mem[p+1:p+4]
-      if inst == 1: mem[t] = mem[a] + mem[b]
-      elif inst == 2: mem[t] = mem[a] * mem[b]
-    p += 4
-  return mem[0]
+class IntCode:
+    def __init__(self, mem):
+      self.mem = mem
+      self.p = 0
+      self.halt = False
+
+    # Program run:
+    def run(self):
+      while not self.halt:
+        # Opcode instruction
+        oc = self.mem[self.p]
+
+        # Addition or multiplication
+        if oc == 1 or oc == 2:
+          a, b, t = self.mem[self.p + 1 : self.p + 4]
+          # Addition:
+          if oc == 1: self.mem[t] = self.mem[a] + self.mem[b]
+          # Multiplication:
+          elif oc == 2: self.mem[t] = self.mem[a] * self.mem[b]
+        self.p += 4
+        if oc == 99: self.halt = True
+      return self.mem[0]
+
 
 # P1:
 memory[1:3] = 12, 2
-print(run(memory))
+prog = IntCode(memory)
+print(prog.run())
 
 # P2:
 res = 19690720
@@ -23,7 +37,8 @@ for a1 in range(100):
   for a2 in range(100):
     memory = dp(memory_)
     memory[1:3] = a1, a2
-    out = run(memory)
+    prog = IntCode(memory)
+    out = prog.run()
     if out == res:
       print(100 * a1 + a2)
       break
