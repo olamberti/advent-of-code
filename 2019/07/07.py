fa7b57a886ca4c305f4690f14fca5d87994cba11
Oliver Lamberti <lamberti.oliver@gmail.com>
2023-05-26 22:09:06 +0200

    updated IntCode class

--- 2019/07/07.py
+++ 2019/07/07.py
@@ -5,69 +5,87 @@ memory = [int(x) for x in open('07.txt').read().split(',')]
 
 class IntCode:
     def __init__(self, mem):
-      self.mem = dp(mem)
-      self.p = 0
-      self.halt = False
+      self.mem = dp(mem)    # internal memory
+      self.pos = 0          # position pointer
+      self.halt = False     # program finished
+      self.commands = {1: 3, 2: 3, 3: 1, 4: 1, 5: 2, 6: 2, 7: 3, 8: 3, 99: 1}
     
-    # Instruction handling
-    def inst(self, n):
+    # Class methods:
+    
+    # 1. Reads opcode and parameter modes
+    def get_opcode(self, n):              
       opcode = n % 100
       n = (n - opcode) // 100
-      pars = []
-      for _ in range(3):
-        pars.append(n % 10)
-        n = (n - pars[-1]) // 10
+      modes = []
+      for _ in range(self.commands[opcode]):
+        modes.append(n % 10)
+        n = (n - modes[-1]) // 10
+      return opcode, modes
+    
+    # 2. Reads parameter based on mode
+    def get_par(self, mode, par):         
+      if mode == 0: return self.mem[par]
+      elif mode == 1: return par
+    
+    # 3. Reads next instruction and its parameters
+    def get_inst(self, pos):              
+      opcode, modes = self.get_opcode(pos)
+      inst = self.mem[self.pos + 1 : self.pos + self.commands[opcode] + 1]
+      if opcode in [1, 2, 7, 8]:
+        pars = [self.get_par(modes[0], inst[0]), self.get_par(modes[1], inst[1]), inst[2]]
+      elif opcode in [5, 6]:
+        pars = [self.get_par(modes[0], inst[0]), self.get_par(modes[1], inst[1])]
+      elif opcode == 3:
+        pars = [inst[0]]
+      elif opcode == 4:
+        pars = [self.get_par(modes[0], inst[0])]
+      elif opcode == 99:
+        pars = None
       return opcode, pars
+        
 
     # Program run
     def run(self, inp):
       while not self.halt:
-        # Opcode instruction
-        oc, pars = self.inst(self.mem[self.p])
+        # Opcode instruction & modes
+        oc, pars = self.get_inst(self.mem[self.pos])
 
         # 1. & 2 Addition or multiplication
         if oc == 1 or oc == 2:
-          p1, p2, t = self.mem[self.p + 1 : self.p + 4]
-          a = p1 if pars[0] == 1 else self.mem[p1]
-          b = p2 if pars[1] == 1 else self.mem[p2]
+          a, b, t = pars[:]
           # Addition
           if oc == 1: self.mem[t] = a + b
           # Multiplication
           elif oc == 2: self.mem[t] = a * b
-          self.p += 4
+          self.pos += 4
         # 3. Input instruction
         elif oc == 3:
-          t = self.mem[self.p + 1]
+          t = pars[0]
           self.mem[t] = inp.pop(0)
-          self.p += 2
+          self.pos += 2
         # 4. Output instruction
         elif oc == 4:
-          p1 = self.mem[self.p + 1]
-          a = p1 if pars[0] == 1 else self.mem[p1]
-          self.p += 2
+          a = pars[0]
+          self.pos += 2
           # Check if program is halt:
-          if self.mem[self.p] == 99:
+          if self.mem[self.pos] == 99:
             self.halt = True
           return a
         # 5. & 6. Jump
         elif oc == 5 or oc == 6:
-          p1, p2 = self.mem[self.p + 1 : self.p + 3]
-          a = p1 if pars[0] == 1 else self.mem[p1]
-          b = p2 if pars[1] == 1 else self.mem[p2]
+          a, b = pars[:]
           # Jump if true
-          if oc == 5: self.p = b if a != 0 else self.p + 3
+          if oc == 5: self.pos = b if a != 0 else self.pos + 3
           # Jump if false
-          elif oc == 6: self.p = b if a == 0 else self.p + 3
+          elif oc == 6: self.pos = b if a == 0 else self.pos + 3
         # 7. & 8. Compare
         elif oc == 7 or oc == 8:
-          p1, p2, t = self.mem[self.p + 1: self.p + 4]
-          a = p1 if pars[0] == 1 else self.mem[p1]
-          b = p2 if pars[1] == 1 else self.mem[p2]
+          a, b, t = pars[:]
           # Less than
           if oc == 7: self.mem[t] = 1 if a < b else 0
           # Equal
           elif oc == 8: self.mem[t] = 1 if a == b else 0
-          self.p += 4
+          self.pos += 4
         # 99. Halt
         elif oc == 99: 
           self.halt = True
--- 2019/11/11.py
+++ 2019/11/11.py
@@ -1,121 +1,144 @@
 # Init:
-memlist = [int(x) for x in open('11.txt').read().split(',')]
-memory = {i: val for i, val in enumerate(memlist)}
+memory = [int(x) for x in open('11.txt').read().split(',')]
 
-def run(mem, p, base, inp):
-  res = []
-  
-  # Read memory:
-  def rmem(n):
-    return mem[n] if n in mem else 0
+class IntCode:
+    def __init__(self, mem):
+      self.mem = {i: val for i, val in enumerate(mem)}  # internal memory as dictionary
+      self.pos = 0          # position pointer
+      self.base = 0         # relative base
+      self.halt = False     # program finished
+      self.commands = {1: 3, 2: 3, 3: 1, 4: 1, 5: 2, 6: 2, 7: 3, 8: 3, 9: 1, 99: 1}
+    
+    # Class methods:
+    
+    # Read memory:
+    def read_mem(self, n):
+        return self.mem[n] if n in self.mem else 0
 
-  while mem[p] != 99:
-    # Handle instruction's opcode:
-    opcode, inst = mem[p], mem[p] % 100
-    opcode = (opcode - inst) // 100
-    pars = []
-    for _ in range(3):
-      pars.append(opcode % 10)
-      opcode = (opcode - pars[-1]) // 10
+    # Read opcode and parameter modes
+    def get_opcode(self, n):              
+      opcode = n % 100
+      n = (n - opcode) // 100
+      modes = []
+      for _ in range(self.commands[opcode]):
+        modes.append(n % 10)
+        n = (n - modes[-1]) // 10
+      return opcode, modes
+    
+    # Read parameter based on mode
+    def get_par(self, mode, par):         
+      if mode == 0: return self.read_mem(par)
+      elif mode == 1: return par
+      elif mode == 2: return self.read_mem(par + self.base)
+    
+    def get_dir(self, mode, dir):
+      if mode == 0: return dir
+      elif mode == 2: return dir + self.base
+    
+    # Reads next instruction and its parameters
+    def get_inst(self, pos):              
+      opcode, modes = self.get_opcode(pos)
+      inst = []
+      for i in range(self.commands[opcode]):
+        inst.append(self.read_mem(self.pos + i + 1))
+      if opcode in [1, 2, 7, 8]:
+        pars = [self.get_par(modes[0], inst[0]), self.get_par(modes[1], inst[1]), self.get_dir(modes[2], inst[2])]
+      elif opcode in [5, 6]:
+        pars = [self.get_par(modes[0], inst[0]), self.get_par(modes[1], inst[1])]
+      elif opcode == 3:
+        pars = [self.get_dir(modes[0], inst[0])]
+      elif opcode in [4, 9]:
+        pars = [self.get_par(modes[0], inst[0])]
+      elif opcode == 99:
+        pars = None
+      return opcode, pars
+        
 
-    # Execute instruction:
-    if inst == 1 or inst == 2:
-      p1, p2, p3 = mem[p+1], mem[p+2], mem[p+3]
-      if pars[0] == 0: a = rmem(p1)
-      elif pars[0] == 1: a = p1
-      elif pars[0] == 2: a = rmem(p1 + base)
-      if pars[1] == 0: b = rmem(p2)
-      elif pars[1] == 1: b = p2
-      elif pars[1] == 2: b = rmem(p2 + base)
-      if pars[2] == 0: t = p3
-      elif pars[2] == 2: t = p3 + base
-      if inst == 1: mem[t] = a + b
-      elif inst == 2: mem[t] = a * b
-      p += 4
-      
-    elif inst == 3:
-      p3 = mem[p+1]
-      if pars[0] == 0: t = p3
-      elif pars[0] == 2: t = p3 + base
-      mem[t] = inp
-      p += 2
-    elif inst == 4:
-      p1 = mem[p+1]
-      if pars[0] == 0: a = rmem(p1)
-      elif pars[0] == 1: a = p1
-      elif pars[0] == 2: a = rmem(p1 + base)
-      res.append(a)
-      p += 2
-    elif inst == 5 or inst == 6:
-      p1, p2 = mem[p+1], mem[p+2]
-      if pars[0] == 0: a = rmem(p1)
-      elif pars[0] == 1: a = p1
-      elif pars[0] == 2: a = rmem(p1 + base)
-      if pars[1] == 0: b = rmem(p2)
-      elif pars[1] == 1: b = p2
-      elif pars[1] == 2: b = rmem(p2 + base)
-      if inst == 5: p = b if a != 0 else p + 3
-      elif inst == 6: p = b if a == 0 else p + 3
-    elif inst == 7 or inst == 8:
-      p1, p2, p3 = mem[p+1], mem[p+2], mem[p+3]
-      if pars[0] == 0: a = rmem(p1)
-      elif pars[0] == 1: a = p1
-      elif pars[0] == 2: a = rmem(p1 + base)
-      if pars[1] == 0: b = rmem(p2)
-      elif pars[1] == 1: b = p2
-      elif pars[1] == 2: b = rmem(p2 + base)
-      if pars[2] == 0: t = p3
-      elif pars[2] == 2: t = p3 + base
-      if inst == 7: mem[t] = 1 if a < b else 0
-      elif inst == 8: mem[t] = 1 if a == b else 0
-      p += 4
-    elif inst == 9:
-      p1 = mem[p+1]
-      if pars[0] == 0: a = rmem(p1)
-      elif pars[0] == 1: a = p1
-      elif pars[0] == 2: a = rmem(p1 + base)
-      base += a
-      p += 2
+    # Program run
+    def run(self, inp):
+      while not self.halt:
+        # Opcode instruction & modes
+        oc, pars = self.get_inst(self.mem[self.pos])
 
-    if len(res) == 2:
-      return res, p, base
-  
-  return res, -1, -1
+        # 1. & 2 Addition or multiplication
+        if oc == 1 or oc == 2:
+          a, b, t = pars[:]
+          # Addition
+          if oc == 1: self.mem[t] = a + b
+          # Multiplication
+          elif oc == 2: self.mem[t] = a * b
+          self.pos += 4
+        # 3. Input instruction
+        elif oc == 3:
+          t = pars[0]
+          self.mem[t] = inp.pop(0)
+          self.pos += 2
+        # 4. Output instruction
+        elif oc == 4:
+          a = pars[0]
+          self.pos += 2
+          # Check if program is halt:
+          if self.mem[self.pos] == 99:
+            self.halt = True
+          return a
+        # 5. & 6. Jump
+        elif oc == 5 or oc == 6:
+          a, b = pars[:]
+          # Jump if true
+          if oc == 5: self.pos = b if a != 0 else self.pos + 3
+          # Jump if false
+          elif oc == 6: self.pos = b if a == 0 else self.pos + 3
+        # 7. & 8. Compare
+        elif oc == 7 or oc == 8:
+          a, b, t = pars[:]
+          # Less than
+          if oc == 7: self.mem[t] = 1 if a < b else 0
+          # Equal
+          elif oc == 8: self.mem[t] = 1 if a == b else 0
+          self.pos += 4
+        # 9. Change relative base
+        elif oc == 9: 
+          a = pars[0]
+          self.base += a
+          self.pos += 2
+        # 99. Halt
+        elif oc == 99: 
+          self.halt = True
+          return None
 
 # P1:
-whites, tile, d = set(), 0, 1j
+whites, visited, tile, d = set(), set(), 0, 1j
+robot = IntCode(memory)
 
-visited, p, base = set(), 0, 0
-while True:
+while not robot.halt:
   visited.add(tile)
   inp = 1 if tile in whites else 0
-  res, p, base = run(memory, p, base, inp)
-  if p == -1: break
-  if res[0] == 0 and tile in whites: whites.remove(tile)
-  elif res[0] == 1: whites.add(tile)
-  if res[1] == 0: d *= 1j 
-  elif res[1] == 1: d *= -1j
+  color = robot.run([inp])
+  turn = robot.run([inp])
+  if color == 0 and tile in whites: whites.remove(tile)
+  elif color == 1: whites.add(tile)
+  if turn == 0: d *= 1j 
+  elif turn == 1: d *= -1j
   tile += d
 print(len(visited))
 
 # P2:
-memory = {i: val for i, val in enumerate(memlist)}
-whites, tile, d = set(), 0, 1j
+whites, visited, tile, d = set(), set(), 0, 1j
 whites.add(tile)
+robot = IntCode(memory)
 
-visited, p, base = set(), 0, 0
 mx, my, Mx, My = float('inf'), float('inf'), 0, 0
-while True:
+while not robot.halt:
   visited.add(tile)
   mx, my = int(min(mx, tile.real)), int(min(my, tile.imag))
   Mx, My = int(max(Mx, tile.real)), int(max(My, tile.imag))
   inp = 1 if tile in whites else 0
-  res, p, base = run(memory, p, base, inp)
-  if p == -1: break
-  if res[0] == 0 and tile in whites: whites.remove(tile)
-  elif res[0] == 1: whites.add(tile)
-  if res[1] == 0: d *= 1j 
-  elif res[1] == 1: d *= -1j 
+  color = robot.run([inp])
+  turn = robot.run([inp])
+  if color == 0 and tile in whites: whites.remove(tile)
+  elif color == 1: whites.add(tile)
+  if turn == 0: d *= 1j 
+  elif turn == 1: d *= -1j
   tile += d
 
 for r in range(My, my - 1, -1):
