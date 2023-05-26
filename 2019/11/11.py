05b926f1f2fcf1bff5ef5dacbf36c27cee48c0c1
Oliver Lamberti <lamberti.oliver@gmail.com>
2023-05-26 23:22:37 +0200

    updated IntCode class

--- 2019/11/11.py
+++ 2019/11/11.py
@@ -4,104 +4,73 @@ memory = [int(x) for x in open('11.txt').read().split(',')]
 class IntCode:
     def __init__(self, mem):
       self.mem = {i: val for i, val in enumerate(mem)}  # internal memory as dictionary
-      self.pos = 0          # position pointer
-      self.base = 0         # relative base
-      self.halt = False     # program finished
-      self.commands = {1: 3, 2: 3, 3: 1, 4: 1, 5: 2, 6: 2, 7: 3, 8: 3, 9: 1, 99: 1}
+      self.pos, self.base, self.halt = 0, 0, False      # position pointer, relative base, program finished
+      self.commands = {1: 3, 2: 3, 3: 1, 4: 1, 5: 2, 6: 2, 7: 3, 8: 3, 9: 1, 99: 0}
     
-    # Class methods:
+    # ----- Class methods ------
     
     # Read memory:
     def read_mem(self, n):
         return self.mem[n] if n in self.mem else 0
 
     # Read opcode and parameter modes
-    def get_opcode(self, n):              
+    def get_inst(self, n):              
       opcode = n % 100
       n = (n - opcode) // 100
-      modes = []
-      for _ in range(self.commands[opcode]):
-        modes.append(n % 10)
-        n = (n - modes[-1]) // 10
-      return opcode, modes
+      self.pos += 1
+      vals, locs = [], []
+      for i in range(self.commands[opcode]):
+        mode = n % 10
+        n = (n - mode) // 10
+        vals.append(self.get_var(mode, self.read_mem(self.pos)))
+        locs.append(self.get_loc(mode, self.read_mem(self.pos)))
+        self.pos += 1
+      return opcode, vals, locs
     
-    # Read parameter based on mode
-    def get_par(self, mode, par):         
+    # Read variable parameters based on mode
+    def get_var(self, mode, par):       
       if mode == 0: return self.read_mem(par)
       elif mode == 1: return par
       elif mode == 2: return self.read_mem(par + self.base)
     
-    # Read target destination based on mode
-    def get_tar(self, mode, dir):
-      if mode == 0: return dir
-      elif mode == 2: return dir + self.base
-    
-    # Reads next instruction and its parameters
-    def get_inst(self, pos):              
-      opcode, modes = self.get_opcode(pos)
-      inst = []
-      for i in range(self.commands[opcode]):
-        inst.append(self.read_mem(self.pos + i + 1))
-      if opcode in [1, 2, 7, 8]:
-        pars = [self.get_par(modes[0], inst[0]), self.get_par(modes[1], inst[1]), self.get_tar(modes[2], inst[2])]
-      elif opcode in [5, 6]:
-        pars = [self.get_par(modes[0], inst[0]), self.get_par(modes[1], inst[1])]
-      elif opcode == 3:
-        pars = [self.get_tar(modes[0], inst[0])]
-      elif opcode in [4, 9]:
-        pars = [self.get_par(modes[0], inst[0])]
-      elif opcode == 99:
-        pars = None
-      return opcode, pars
+    # Read target parameter based on mode
+    def get_loc(self, mode, par):
+      if mode == 0: return par
+      elif mode == 1: return None
+      elif mode == 2: return par + self.base
         
-
     # Program run
     def run(self, inp):
       while not self.halt:
         # Opcode instruction & modes
-        oc, pars = self.get_inst(self.mem[self.pos])
+        oc, vals, locs = self.get_inst(self.mem[self.pos])
 
         # 1. & 2 Addition or multiplication
         if oc == 1 or oc == 2:
-          a, b, t = pars[:]
-          # Addition
-          if oc == 1: self.mem[t] = a + b
-          # Multiplication
-          elif oc == 2: self.mem[t] = a * b
-          self.pos += 4
+          if oc == 1: self.mem[locs[2]] = vals[0] + vals[1]
+          elif oc == 2: self.mem[locs[2]] = vals[0] * vals[1]
         # 3. Input instruction
         elif oc == 3:
-          t = pars[0]
-          self.mem[t] = inp.pop(0)
-          self.pos += 2
+          self.mem[locs[0]] = inp.pop(0)
         # 4. Output instruction
         elif oc == 4:
-          a = pars[0]
-          self.pos += 2
           # Check if program is halt:
           if self.mem[self.pos] == 99:
             self.halt = True
-          return a
+          return vals[0]
         # 5. & 6. Jump
         elif oc == 5 or oc == 6:
-          a, b = pars[:]
-          # Jump if true
-          if oc == 5: self.pos = b if a != 0 else self.pos + 3
-          # Jump if false
-          elif oc == 6: self.pos = b if a == 0 else self.pos + 3
+          if oc == 5: 
+            if vals[0] != 0: self.pos = vals[1] 
+          elif oc == 6: 
+            if vals[0] == 0: self.pos = vals[1] 
         # 7. & 8. Compare
         elif oc == 7 or oc == 8:
-          a, b, t = pars[:]
-          # Less than
-          if oc == 7: self.mem[t] = 1 if a < b else 0
-          # Equal
-          elif oc == 8: self.mem[t] = 1 if a == b else 0
-          self.pos += 4
+          if oc == 7: self.mem[locs[2]] = 1 if vals[0] < vals[1] else 0
+          elif oc == 8: self.mem[locs[2]] = 1 if vals[0] == vals[1] else 0
         # 9. Change relative base
         elif oc == 9: 
-          a = pars[0]
-          self.base += a
-          self.pos += 2
+          self.base += vals[0]
         # 99. Halt
         elif oc == 99: 
           self.halt = True
