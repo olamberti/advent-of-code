117399046f4a013a0b442ba8c73cec96dd497ebc
olamberti <olambert@volvocars.com>
2023-08-17 13:56:03 +0200

    2019 solutions

--- 2019/16.py
+++ 2019/16.py
@@ -1,24 +1,37 @@
-sig = open('16.txt').read()
+from copy import deepcopy as dp
+
+sig = [int(x) for x in open('16.txt').read()]
 base = [0, 1, 0, -1]
 
-def fft(sig):
-    out = ""
-    for k in range(1, len(sig) + 1):
+# P1
+def fft(inp):
+    out = []
+    for k in range(1, len(inp) + 1):
         pattern = sum([k * [n] for n in base], [])
         val = 0
-        for i, x in enumerate(sig):
+        for i, x in enumerate(inp):
             w = pattern[(i + 1) % len(pattern)]
-            val += int(x) * w
-        out += str(abs(val) % 10)
+            val += x * w
+        out.append(abs(val) % 10)
     return out
 
-#P1
-p1 = sig
+p1 = dp(sig)
 for _ in range(100):
     p1 = fft(p1)
-print(p1[:8])
+print(''.join(str(x) for x in p1[:8]))
 
-#P2
+# P2
+def ffs(inp):
+    out, val = [], 0
+    for x in inp[::-1]:
+        val = (val + x) % 10
+        out.append(val)
+    return out[::-1]
+
 N = 10_000
-offset = int(sig[:8])
-print(offset)
+offset, p2 = int(''.join(str(x) for x in sig[:7])), dp(sig) * N
+p2 = p2[offset:]
+
+for _ in range(100):
+    p2 = ffs(p2)
+print(''.join(str(x) for x in p2[:8]))
