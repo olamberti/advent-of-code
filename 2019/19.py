9b561046e3275783a3d11c6d42c5d564995cea53
Oliver Lamberti <lamberti.oliver@gmail.com>
2023-08-22 22:33:05 +0200

    2019 solutions

--- 2019/19.py
+++ 2019/19.py
@@ -1,18 +1,26 @@
 from intcode import IntCode
 memory = [int(x) for x in open('19.txt').read().split(',')]
 
+def printmap(beam):
+    MX, MY = int(max([n.real for n in beam])), int(max([n.imag for n in beam]))
+    grid = ''
+    for x in range(MX + 1):
+        for y in range(MY + 1):
+            if x + y*1j in beam: grid += '#'
+            else: grid += '.'
+        grid += '\n'
+    print(grid)
+
 # P1
-beam, grid, size = set(), '', 50
+beam, size = set(), 50
 for x in range(size):
     for y in range(size):
         prog = IntCode((memory))
         if prog.run([x, y]):
             beam.add(x + y*1j)
-            grid += '#'
-        else: grid += '.'
-    grid += '\n'
 print(len(beam))
-print(grid)
+printmap(beam)
 
+pass
 # P2
 # to be implemented
