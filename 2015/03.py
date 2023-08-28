5852ee695f1d89427a7376e3796967d258cd6ebc
Oliver Lamberti <lamberti.oliver@gmail.com>
2023-08-28 23:34:30 +0200

    2015 solutions

---
+++ 2015/03.py
@@ -0,0 +1,17 @@
+dirs = {'^': 1j, 'v': -1j, '>': 1, '<': -1}
+
+# P1
+houses_p1, pos = set(), 0
+houses_p2, pos1, pos2 = set(), 0, 0
+houses_p1.add(0); houses_p2.add(0)
+for i, d in enumerate(open('03.txt').read()):
+    pos += dirs[d]
+    houses_p1.add(pos)
+    if i % 2 == 0:
+        pos1 += dirs[d]
+        houses_p2.add(pos1)
+    else:
+        pos2 += dirs[d]
+        houses_p2.add(pos2)
+print(len(houses_p1))
+print(len(houses_p2))
