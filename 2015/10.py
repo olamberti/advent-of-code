ddb8028378b857a6facf22c1ffce3ec59a73351c
olamberti <olambert@volvocars.com>
2023-08-30 15:43:08 +0200

    2015 solutions

--- 2015/10.py
+++ 2015/10.py
@@ -1,22 +1,10 @@
+from itertools import groupby as gb
 num = open('10.txt').read()
 
-def play(num):
-    res, i, n = '', 0, 1
-    while i < len(num):
-        old = num[i]
-        if i + 1 < len(num):
-            new = num[i + 1]
-            if old == new: n += 1
-            else:
-                res += str(n) + old
-                n = 1
-        else: res += str(n) + old
-        i += 1
-    return res
+def lookandsay(s):
+    return ''.join([str(sum(1 for _ in g)) + k for k, g in gb(s)])
 
 for i in range(50):
-    num = play(num)
+    num = lookandsay(num)
     if i == 39: print(len(num))
 print(len(num))
-
-# Runs slow for P2, needs an update...
