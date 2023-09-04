197401dd2cc44b25364a51652227a9e4aaa049c8
Oliver Lamberti <lamberti.oliver@gmail.com>
2023-09-04 23:44:02 +0200

    2015 solutions

--- 2015/20.py
+++ 2015/20.py
@@ -5,8 +5,11 @@ tar = int(open('20.txt').read())
 def presents(n):
     return sum(sympy.divisors(n)) * 10
 
-# P1
+# P1 - TODO optimize instead of BF
 n = 1
-while presents(n) < tar:
-    n += 1
+while presents(n) < tar: n += 1
 print(n)
+
+# P2
+# TODO code part 2
+
