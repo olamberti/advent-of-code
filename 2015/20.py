f2a9e115d7b27106e0fd72fdd7f97b46dccbf13f
olamberti <olambert@volvocars.com>
2023-09-05 15:38:27 +0200

    2015 solutions

--- 2015/20.py
+++ 2015/20.py
@@ -4,11 +4,18 @@ import numpy as np
 
 lim = int(open('20.txt').read())
 
+# Theory based on https://en.wikipedia.org/wiki/Divisor_function
+def ub(n):
+    return (pow(np.e, np.euler_gamma) * n * math.log(math.log(n))) * 10
+
+start = 2
+while ub(start) < lim: start += 1
+
 # P1
 def p1(n):
     return math.prod([int((pow(p, a + 1) - 1) / (p - 1)) for p, a in sympy.factorint(n).items()]) * 10
 
-n = 1
+n = start
 while p1(n) < lim: n += 1
 print(n)
 
@@ -20,6 +27,6 @@ def p2(n):
         res += d * 11
     return res
 
-n = 1
+n = start
 while p2(n) < lim: n += 1
 print(n)
