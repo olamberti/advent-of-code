24eb6e66359867af95c5aae678ecfa25755fb54e
olamberti <olambert@volvocars.com>
2023-09-05 11:14:54 +0200

    2015 solutions

--- 2015/20.py
+++ 2015/20.py
@@ -1,15 +1,25 @@
 import sympy
+import math
+import numpy as np
 
-tar = int(open('20.txt').read())
+lim = int(open('20.txt').read())
 
-def presents(n):
-    return sum(sympy.divisors(n)) * 10
+# P1
+def p1(n):
+    return math.prod([int((pow(p, a + 1) - 1) / (p - 1)) for p, a in sympy.factorint(n).items()]) * 10
 
-# P1 - TODO optimize instead of BF
 n = 1
-while presents(n) < tar: n += 1
+while p1(n) < lim: n += 1
 print(n)
 
 # P2
-# TODO code part 2
+def p2(n):
+    res, divs = 1, sympy.divisors(n)
+    for d in divs:
+        if n / d > 50: continue
+        res += d * 11
+    return res
 
+n = 1
+while p2(n) < lim: n += 1
+print(n)
