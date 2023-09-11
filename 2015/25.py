440163241f6767c724319e9b200d67cf7231fe6f
Oliver Lamberti <lamberti.oliver@gmail.com>
2023-09-11 23:08:51 +0200

    2015 solutions

--- 2015/25.py
+++ 2015/25.py
@@ -1,5 +1,6 @@
 import re
 
+b, mod = 252533, 33554393
 r, c = [int(x) for x in re.findall(r'\d+', open('25.txt').read())]
 code, i = 20151125, (r + c - 1)*(r + c) // 2 - r
-print(code * pow(252533, i, 33554393) % 33554393)
+print(code * pow(b, i, mod) % mod)
