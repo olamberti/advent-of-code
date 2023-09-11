40cb70ab98f5c0eb75e119eebd9bb726542d1460
Oliver Lamberti <lamberti.oliver@gmail.com>
2023-09-11 23:06:41 +0200

    2015 solutions

--- 2015/25.py
+++ 2015/25.py
@@ -1,10 +1,5 @@
 import re
-R, C = [int(x) for x in re.findall(r'\d+', open('25.txt').read())]
 
-r, c, code = 1, 1, 20151125
-
-while not(r == R and c == C):
-    r , c = r - 1, c + 1
-    if r == 0: r, c = c, 1
-    code = code * 252533 % 33554393
-print(code)
+r, c = [int(x) for x in re.findall(r'\d+', open('25.txt').read())]
+code, i = 20151125, (r + c - 1)*(r + c) // 2 - r
+print(code * pow(252533, i, 33554393) % 33554393)
