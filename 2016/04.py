ccc87d7f057054090de9fcc99e69bffe18eff3f3
olamberti <olambert@volvocars.com>
2023-09-12 14:06:40 +0200

    2016 solutions

--- 2016/04.py
+++ 2016/04.py
@@ -1,4 +1,4 @@
-def shift(c, n):
+def shift(c, n):  # (a - 97, z - 122)
     return chr((ord(c) - 97 + n) % 26 + 97)
 
 p1, p2 = 0, None
@@ -20,7 +20,7 @@ for line in open('04.txt').read().splitlines():
     for i in range(len(check)): to_check += chars[i][0]
     if check == to_check: p1 += sID
 
-    # P2 (a - 97, z - 122)
+    # P2
     rname = ''
     for sname in name:
         for c in sname:
