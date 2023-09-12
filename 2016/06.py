53cf8d19589f34485ce89d2ad683d392093530a8
olamberti <olambert@volvocars.com>
2023-09-12 15:11:31 +0200

    2016 solutions

---
+++ 2016/06.py
@@ -0,0 +1,12 @@
+chars = [{}, {}, {}, {}, {}, {}, {}, {}]
+for line in open('06.txt').read().splitlines():
+    for i, c in enumerate(line.strip()):
+        if c not in chars[i]: chars[i][c] = 1
+        else: chars[i][c] += 1
+
+p1, p2 = '', ''
+for d in chars:
+    p1 += sorted([(v, k) for k, v in d.items()], reverse=True)[0][1]
+    p2 += sorted([(v, k) for k, v in d.items()])[0][1]
+print(p1)
+print(p2)
