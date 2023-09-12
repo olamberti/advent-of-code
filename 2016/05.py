688398c858e1ec4ceb4d36469f67172305be9078
olamberti <olambert@volvocars.com>
2023-09-12 14:36:52 +0200

    2016 solutions

---
+++ 2016/05.py
@@ -0,0 +1,14 @@
+import hashlib
+dID = open('05.txt').read()
+
+i, p1, p2 = 0, '', ['?']*8
+while len(p1) < 8 or '?' in p2:
+    hash = hashlib.md5((dID + str(i)).encode("utf-8")).hexdigest()
+    if hash[:5] == '00000':
+        if len(p1) < 8: p1 += hash[5]
+        if hash[5] in '01234567':
+            p = int(hash[5])
+            if p2[p] == '?': p2[p] = hash[6]
+    i += 1
+print(p1)
+print(''.join(p2))
