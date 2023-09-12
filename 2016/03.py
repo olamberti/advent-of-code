e43aaadb34d82c407c1ba4a0c356fbd7dace4ade
olamberti <olambert@volvocars.com>
2023-09-12 11:53:10 +0200

    2016 solutions

---
+++ 2016/03.py
@@ -0,0 +1,17 @@
+def tri(a, b, c):
+    a, b, c = sorted([a, b, c])
+    return a + b > c
+
+p1, p2 = 0, 0
+for i, line in enumerate(open('03.txt').read().splitlines()):
+    if i % 3 == 0: A, B, C = [], [], []
+    a, b, c = [int(x) for x in line.strip().split()]
+    if tri(a, b, c): p1 += 1
+    A.append(a); B.append(b); C.append(c)
+    if i % 3 == 2:
+        if tri(*A): p2 += 1
+        if tri(*B): p2 += 1
+        if tri(*C): p2 += 1
+        
+print(p1)
+print(p2)
