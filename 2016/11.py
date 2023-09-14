e1c9b7bce36f478da1005cd24d0070fe72ff1b0a
olamberti <olambert@volvocars.com>
2023-09-14 10:42:48 +0200

    2016 solutions

--- 2016/11.py
+++ 2016/11.py
@@ -4,19 +4,18 @@ from copy import deepcopy as dp
 from collections import deque as dq
 
 # Init
-floors, num, mats = [], 0, set()
+floors, mats = [], set()
 for line in open('11.txt').read().splitlines():
     floor = []
     items = re.findall(r' (\w+|\w+-compatible) (microchip|generator)', line)
     for m, t in items:
         mat = m[0].upper() + m[1]
         floor.append(mat + t[0].upper())
-        num += 1
         mats.add(mat)
     floors.append(floor)
 
 def create_status(elevator, floors):
-    return (elevator, tuple(tuple(sorted(f)) for f in floors))
+    return (elevator, tuple(tuple(f) for f in floors))
 
 def fried(items):
     if len(items) == 0 or len(items) == 1: return False
@@ -32,6 +31,8 @@ def next_statuses(status):
     for d in [-1, 1]:
         ne = e + d
         if ne < 0 or 3 < ne: continue
+        if len(floors[0]) == 0 and ne == 0: continue
+        if (len(floors[0]) == 0 and len(floors[1]) == 0) and ne == 1: continue
         items = floors[e]
         for n in [1, 2]:
             for take in combinations(items, n):
@@ -44,36 +45,34 @@ def next_statuses(status):
                 res.append(create_status(ne, nfloors))
     return res
 
-def status_combo(status):
-    res, e, floors = set(), status[0], list([list(f) for f in status[1]])
-    for m1, m2 in combinations(mats, 2):
-        nfloors = [[f.replace(m1, 'XX') for f in l] for l in floors]
-        nfloors = [[f.replace(m2, m1) for f in l] for l in nfloors]
-        nfloors = [[f.replace('XX', m2) for f in l] for l in nfloors]
-        res.add(create_status(e, nfloors))
+def cache_hash(status):
+    res, pairs, e, floors = set(), {}, status[0], list([list(f) for f in status[1]])
+    for mat in mats: pairs[mat] = [None, None]
+    for i, floor in enumerate(floors):
+        for item in floor:
+            mat, t = item[:-1], item[-1]
+            if t == 'M': pairs[mat][0] =  i
+            else: pairs[mat][1] =  i
+    res = (e, tuple([tuple(p) for p in sorted(pairs.values())]))
     return res
 
 def min_steps(start):
     stack, cache = dq([[0, start]]), set()
-    cache.add(start)
-    for sts in status_combo(start):
-        cache.add(sts)
+    cache.add(cache_hash(start))
     while True:
         steps, status = stack.popleft()
         steps += 1
         for nstatus in next_statuses(status):
-            if nstatus in cache: continue
-            cache.add(nstatus)
-            for nsts in status_combo(nstatus):
-                cache.add(nsts)
-            if len(nstatus[1][-1]) == num:
+            if cache_hash(nstatus) in cache: continue
+            cache.add(cache_hash(nstatus))
+            if len(nstatus[1][-1]) == 2 * len(mats):
                 return steps
             stack.append([steps, nstatus])
 
 # P1
 print(min_steps(create_status(0, floors)))
 
-# P2 - TODO too slow, needs opting
+# P2
 floors[0] += ['ElG', 'ElM', 'DiG', 'DiM']
 mats.add('El'), mats.add('Di')
 print(min_steps(create_status(0, floors)))
