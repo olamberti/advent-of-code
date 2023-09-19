3a61e30fd4b56fdee54159f3611a52476f85085a
olamberti <olambert@volvocars.com>
2023-09-19 16:39:58 +0200

    2016 solutions

---
+++ 2016/18.py
@@ -0,0 +1,15 @@
+grid = []
+grid.append(open('18.txt').read())
+
+safe = grid[0].count('.')
+while len(grid) < 400_000:
+    line, nline = '.' + grid[-1] + '.', ''
+    for j in range(1, len(line) - 1):
+        if line[j-1 : j+2] in ['^^.', '.^^', '^..','..^']:
+            nline += '^'
+        else:
+            nline += '.'
+            safe += 1
+    grid.append(nline)
+    if len(grid) == 40: print(safe)
+print(safe)
