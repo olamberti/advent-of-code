40b2d28ede2ddc02188292e49d462aed61ae1a2a
Oliver Lamberti <lamberti.oliver@gmail.com>
2023-09-20 22:10:59 +0200

    2016 solutions

--- 2016/19.py
+++ 2016/19.py
@@ -1,11 +1,11 @@
 import math
 n = int(open('19.txt').read())
 
-
 # P1
 print(int('0b' + bin(n)[3:] + '1', 2))
 
 # P2
 t = int(pow(3, math.floor(math.log(n, 3))))
-if n < t * 2: print(n - t)
+if n == t: print(n)
+elif n < t * 2: print(n - t)
 else: print(2 * n - 3 * t)
