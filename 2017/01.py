7742cda6ea6785c5e76db356c32dd87a8562862c
olamberti <olambert@volvocars.com>
2023-09-22 12:50:25 +0200

    2017 solutions

---
+++ 2017/01.py
@@ -0,0 +1,10 @@
+inp = [int(x) for x in open('01.txt').read()]
+
+# P1
+p1, p2 = 0, 0
+d, m = len(inp) // 2, len(inp)
+for i in range(len(inp)):
+    if inp[i] == inp[(i + 1) % m]: p1 += inp[i]
+    if inp[i] == inp[(i + d) % m]: p2 += inp[i]
+print(p1)
+print(p2)
---
+++ 2017/02.py
@@ -0,0 +1,12 @@
+import re
+
+p1, p2 = 0, 0
+for line in open('02.txt').read().splitlines():
+    v = [int(x) for x in re.findall('\d+', line)]
+    p1 += max(v) - min(v)
+    for a in v:
+        for b in v:
+            if a == b: continue
+            if a % b == 0: p2 += a // b
+print(p1)
+print(p2)
