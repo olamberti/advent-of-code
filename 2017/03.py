be12208c6c1c66f3d0ebed7d70bb8421d86ef30c
olamberti <olambert@volvocars.com>
2023-09-25 11:32:25 +0200

    2017 solutions

--- 2017/03.py
+++ 2017/03.py
@@ -1,16 +1,18 @@
 inp = int(open('03.txt').read())
 
 # P1
-sq, p = 1, 0
-while sq ** 2  < inp:
-    sq += 2; p += 1 + 1j
-n = sq ** 2
+def p1(inp):
+    sq, p = 1, 0
+    while sq ** 2  < inp:
+        sq += 2; p += 1 + 1j
+    n = sq ** 2
 
-for d in [-1, -1j, 1, 1j]:
-    for _ in range(1, sq):
-        if n == inp:
-            print(int(abs(p.real) + abs(p.imag)))
-        n -= 1; p += d
+    for d in [-1, -1j, 1, 1j]:
+        for _ in range(1, sq):
+            if n == inp:
+                return int(abs(p.real) + abs(p.imag))
+            n -= 1; p += d
+print(p1(inp))
 
 # P2
 def nsum(pos, vals):
@@ -20,15 +22,16 @@ def nsum(pos, vals):
         if np in vals: res += vals[np]
     return res
 
-pos, vals, i, steps, p2 = 0, {}, 0, 0, True
-vals[pos] = 1
-while p2:
-    for d in [1, -1j, -1, 1j]:
-        if i % 2 == 0: steps += 1
-        for _ in range(steps):
-            pos = pos + d
-            vals[pos] = nsum(pos, vals)
-            if vals[pos] > inp and p2:
-                p2 = False
-                print(vals[pos])
-        i += 1
+def p2(inp):
+    pos, vals, i, steps = 0, {}, 0, 0
+    vals[pos] = 1
+    while True:
+        for d in [1, -1j, -1, 1j]:
+            if i % 2 == 0: steps += 1
+            for _ in range(steps):
+                pos = pos + d
+                vals[pos] = nsum(pos, vals)
+                if vals[pos] > inp:
+                    return vals[pos]
+            i += 1
+print(p2(inp))
