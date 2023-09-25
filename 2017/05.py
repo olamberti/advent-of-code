1fcbf8ad5123dccd8f3789decb71e382d2271764
olamberti <olambert@volvocars.com>
2023-09-25 14:02:31 +0200

    2017 solutions

---
+++ 2017/05.py
@@ -0,0 +1,17 @@
+from copy import deepcopy as dp
+data = [int(x) for x in open('05.txt').read().splitlines()]
+
+def run(input, p2 = False):
+    data = dp(input)
+    i, steps = 0, 0
+    while i < len(data):
+        ni = i + data[i]
+        if p2 and data[i] >= 3: data[i] -= 1
+        else: data[i] += 1
+        i = ni
+        steps += 1
+    return(steps)
+
+
+print(run(data))
+print(run(data, True))
