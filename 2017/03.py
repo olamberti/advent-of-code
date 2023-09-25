da068007f24dd01de562d7dede28866d01229222
olamberti <olambert@volvocars.com>
2023-09-25 11:26:47 +0200

    2017 solutions

--- 2017/03.py
+++ 2017/03.py
@@ -6,12 +6,29 @@ while sq ** 2  < inp:
     sq += 2; p += 1 + 1j
 n = sq ** 2
 
-dirs = [-1, -1j, 1, 1j]
-for d in dirs:
+for d in [-1, -1j, 1, 1j]:
     for _ in range(1, sq):
         if n == inp:
             print(int(abs(p.real) + abs(p.imag)))
         n -= 1; p += d
 
 # P2
-# TODO: prog part 2
+def nsum(pos, vals):
+    res = 0
+    for d in [1, 1-1j, -1j, -1-1j, -1, -1+1j, 1j, 1+1j]:
+        np = pos + d
+        if np in vals: res += vals[np]
+    return res
+
+pos, vals, i, steps, p2 = 0, {}, 0, 0, True
+vals[pos] = 1
+while p2:
+    for d in [1, -1j, -1, 1j]:
+        if i % 2 == 0: steps += 1
+        for _ in range(steps):
+            pos = pos + d
+            vals[pos] = nsum(pos, vals)
+            if vals[pos] > inp and p2:
+                p2 = False
+                print(vals[pos])
+        i += 1
