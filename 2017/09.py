c46b386c1dba56cd3d4c4151e1899546450a49af
olamberti <olambert@volvocars.com>
2023-09-27 09:25:24 +0200

    2017 solutions

---
+++ 2017/09.py
@@ -0,0 +1,16 @@
+data = open('09.txt').read()
+
+i, lvl, garbage, p1, p2 = 0, 0, False, 0, 0
+while i < len(data):
+    if not garbage:
+        if data[i] == '<': garbage = True
+        elif data[i] == '{': lvl += 1
+        elif data[i] == '}': p1 += lvl; lvl -= 1
+    else:
+        if data[i] == '!': i += 1
+        elif data[i] == '>': garbage = False
+        else: p2 += 1
+    i += 1
+
+print(p1)
+print(p2)
