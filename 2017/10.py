3f97683f1f4c48f884db6defe7a74e30637d476d
olamberti <olambert@volvocars.com>
2023-09-28 14:43:28 +0200

    2017 solutions

--- 2017/10.py
+++ 2017/10.py
@@ -1,17 +1,39 @@
-c, lengths = [i for i in range(256)], [int(x) for x in open('10.txt').read().split(',')]
+from copy import deepcopy as dp
 
 # P1
-i, ss = 0, 0
-for l in lengths:
-    if i + l <= len(c):
-        c[i:i+l] = c[i:i+l][::-1]
-    else:
-        l1, l2 = len(c[i:]), len(c[:(i+l)-len(c)])
-        cut = (c[i:] + c[:(i+l)-len(c)])[::-1]
-        c[i:], c[:(i+l)-len(c)] = cut[:l1], cut[l1:]
-    i = (i + l + ss) % len(c)
-    ss += 1
+inp = open('10.txt').read()
+c, lengths = [i for i in range(256)], [int(x) for x in inp.split(',')]
+
+def round(c, i, ss, ls):
+    for l in ls:
+        if i + l <= len(c):
+            c[i:i+l] = c[i:i+l][::-1]
+        else:
+            l1 = len(c[i:])
+            cut = (c[i:] + c[:(i+l)-len(c)])[::-1]
+            c[i:], c[:(i+l)-len(c)] = cut[:l1], cut[l1:]
+        i = (i + l + ss) % len(c)
+        ss += 1
+    return c, i, ss
+
+c, _, _ = round(c, 0, 0, lengths)
 print(c[0] * c[1])
 
 # P2
-# TODO
+def knothash(s):
+    c, lengths, i, ss = [i for i in range(256)], [ord(x) for x in s] + [17, 31, 73, 47, 23], 0, 0
+    for _ in range(64):
+        c, i, ss = round(c, i, ss, lengths)
+
+    dh = []
+    for i in range(16):
+        t = c[16 * i]
+        for j in range(15): t = t ^ c[16 * i + j]
+        dh.append(t)
+    
+    res = ''
+    for x in dh:
+        res += hex(x)[2:].zfill(2)
+    return res
+
+print(knothash('AoC 2015'))
