2699178c86e03476ec601d8e7e0b7ae1ded07836
olamberti <olambert@volvocars.com>
2023-10-03 13:38:33 +0200

    2017 solutions

---
+++ 2017/13.py
@@ -0,0 +1,25 @@
+fw = {}
+for line in open('13.txt').read().splitlines(0):
+    d, r = [int(x) for x in line.split(': ')]
+    fw[d] = r
+
+# P1
+def getpos(r, t):
+    m = t % (r - 1)
+    if t // (r - 1) % 2 == 0: return m
+    else: return r - m
+
+sev = 0
+for d, r in fw.items():
+    if getpos(r, d) == 0: sev += r * d
+print(sev)
+
+# P2
+p2 = 0
+while True:
+    for d, r in fw.items():
+        if getpos(r, d + p2) == 0: break
+    else:
+        print(p2)
+        break
+    p2 += 1
