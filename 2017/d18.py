7e01c122a498bdf94d8f58bfcfe145496304eb6c
olamberti <olambert@volvocars.com>
2023-10-11 15:46:56 +0200

    2017 solutions

--- 2017/d18.py
+++ 2017/d18.py
@@ -1,84 +1,74 @@
-prog = [line for line in open('d18.txt').read().splitlines()]
-
+from collections import defaultdict
+prog = [line.split() for line in open('d18.txt').read().splitlines()]
 
 # P1
-regs = {}
-def get_val(x):
+regs = defaultdict(int)
+def get(x):
     if x.lstrip('-').isnumeric(): return int(x)
-    if x in regs: return regs[x]
-    regs[x] = 0
-    return 0
+    return regs[x]
 
 pos, sound, rec = 0, None, None
 while 0 <= pos < len(prog):
-    ins = prog[pos].split()
-    if ins[0] == 'snd': sound = get_val(ins[1])
-    elif ins[0] == 'set':
-        if ins[1] not in regs: regs[ins[1]] = 0
-        regs[ins[1]] = get_val(ins[2])
-    elif ins[0] == 'add':
-        if ins[1] not in regs: regs[ins[1]] = 0
-        regs[ins[1]] += get_val(ins[2])
-    elif ins[0] == 'mul':
-        if ins[1] not in regs: regs[ins[1]] = 0
-        regs[ins[1]] *= get_val(ins[2])
-    elif ins[0] == 'mod':
-        if ins[1] not in regs: regs[ins[1]] = 0
-        regs[ins[1]] %= get_val(ins[2])
+    ins = prog[pos]
+    if ins[0] == 'snd': sound = get(ins[1])
+    elif ins[0] == 'set': regs[ins[1]] = get(ins[2])
+    elif ins[0] == 'add': regs[ins[1]] += get(ins[2])
+    elif ins[0] == 'mul': regs[ins[1]] *= get(ins[2])
+    elif ins[0] == 'mod': regs[ins[1]] %= get(ins[2])
     elif ins[0] == 'rcv':
-        if get_val(ins[1]) != 0:
+        if get(ins[1]) != 0:
             rec = sound
             break
     elif ins[0] == 'jgz':
-        if get_val(ins[1]) != 0: pos += get_val(ins[2]) - 1
+        if get(ins[1]) > 0: pos += get(ins[2]) - 1
     pos += 1
 
 print(rec)
 
 # P2
-# TODO finish class and run two programs
 class Prog():
 
-    def __init__(self, prog, num):
-        self.prog = prog
-        self.regs = {'p': num}
+    def __init__(self, num):
+        self.regs = defaultdict(int)
+        self.regs['p'] = num
         self.pos = 0
         self.queue = []
         self.halt = False
+        self.sent = 0
     
-    def get_reg(self, x):
+    def get(self, x):
         if x.lstrip('-').isnumeric(): return int(x)
-        if x in self.regs: return self.regs[x]
-        self.regs[x] = 0
-        return 0
+        return self.regs[x]
         
     def run(self):
         self.halt, pack = False, []
         while not self.halt:
-            if 0 <= self.pos < len(prog):
+            if self.pos < 0 or self.pos >= len(prog):
                 self.halt = True
                 break
 
-            ins = prog[self.pos].split()
-            if ins[0] == 'snd': pack.append(self.get_reg(ins[1]))
+            ins = prog[self.pos]
+            if ins[0] == 'snd':
+                pack.append(self.get(ins[1]))
+                self.sent += 1
             elif ins[0] == 'rcv':
-                if ins[1] not in self.regs: self.regs[ins[1]] = 0
                 if self.queue: self.regs[ins[1]] = self.queue.pop(0)
                 else:
                     self.halt = True
                     break
-            elif ins[0] == 'set':
-                if ins[1] not in self.regs: self.regs[ins[1]] = 0
-                self.regs[ins[1]] = self.get_reg(ins[2])
-            elif ins[0] == 'add':
-                if ins[1] not in self.regs: self.regs[ins[1]] = 0
-                self.regs[ins[1]] += self.get_reg(ins[2])
-            elif ins[0] == 'mul':
-                if ins[1] not in self.regs: self.regs[ins[1]] = 0
-                self.regs[ins[1]] *= self.get_reg(ins[2])
-            elif ins[0] == 'mod':
-                if ins[1] not in self.regs: self.regs[ins[1]] = 0
-                self.regs[ins[1]] %= self.get_reg(ins[2])   
+            elif ins[0] == 'set': self.regs[ins[1]] = self.get(ins[2])
+            elif ins[0] == 'add': self.regs[ins[1]] += self.get(ins[2])
+            elif ins[0] == 'mul': self.regs[ins[1]] *= self.get(ins[2])
+            elif ins[0] == 'mod': self.regs[ins[1]] %= self.get(ins[2])   
             elif ins[0] == 'jgz':
-                if self.get_reg(ins[1]) != 0: self.pos += self.get_reg(ins[2]) - 1
+                if self.get(ins[1]) > 0: self.pos += self.get(ins[2]) - 1
             self.pos += 1
+        return pack
+    
+progs, act = [Prog(0), Prog(1)], 0
+
+while (progs[0].halt == False or progs[1].halt == False) or (len(progs[0].queue) + len(progs[1].queue) > 0):
+    progs[1-act].queue.extend(progs[act].run())
+    act = 1 - act
+
+print(progs[1].sent)
