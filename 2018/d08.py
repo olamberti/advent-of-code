f7990ae2075c4eb5e4eed58f15370b2949bd41a3
Oliver Lamberti <lamberti.oliver@gmail.com>
2023-11-29 21:53:43 +0100

    2018 solutions

--- 2018/d08.py
+++ 2018/d08.py
@@ -1,7 +1,19 @@
 data = [int(x) for x in open('d08.txt').read().split()]
 
+class Node():
+    def __init__(self, n_child, n_meta, parent):
+        self.nCh = n_child
+        self.nM = n_meta
+        self.parent = parent
+        self.children = None
+
 # P1:
-# TODO code part 1
+tree = Node(data[0], data[1], None)
+
+i = 2
+while i < len(data):
+    pass
+
 
 # P2:
 # TODO code part 2
