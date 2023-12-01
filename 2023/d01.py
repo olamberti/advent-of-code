8abceec7f82772dd71dd8488ba7bffed0bfbab4b
olamberti <olambert@volvocars.com>
2023-12-01 16:33:20 +0100

    2023 solutions

--- 2023/d01.py
+++ 2023/d01.py
@@ -1,19 +1,19 @@
-import regex as re
+import re
+
+numbers = ['one','two','three','four','five','six','seven','eight','nine']
+
+def convert(s):
+    return str(numbers.index(s) + 1) if s in numbers else s
 
 p1, p2 = 0, 0
-num2dig = {'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5',
-            'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
 
 for line in open('d01.txt').read().splitlines():
     # Part 1
     digits = re.findall(r'\d', line)
     p1 += int(digits[0] + digits[-1])
-    
     # Part 2
-    digits = re.findall(r'\d|one|two|three|four|five|six|seven|eight|nine', line, overlapped=True)
-    first = num2dig[digits[0]] if digits[0] in num2dig else digits[0]
-    last = num2dig[digits[-1]] if digits[-1] in num2dig else digits[-1]
-    p2 += int(first + last)
+    digits = re.findall(r'(?=(\d|' + '|'.join(numbers) + '))', line)
+    p2 += int(convert(digits[0]) + convert(digits[-1]))
 
 print(p1)
 print(p2)
