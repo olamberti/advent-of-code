a668d543c2bf8eebcee50f685a91718e017eb026
Oliver Lamberti <lamberti.oliver@gmail.com>
2023-12-02 06:55:27 +0100

    2023  solutions

--- 2023/d02.py
+++ 2023/d02.py
@@ -5,11 +5,10 @@ MAXES = {'red':12, 'green':13, 'blue':14}
 
 p1, p2 = 0, 0
 for id, line in enumerate(open('d02.txt').read().splitlines(), 1):
-    pairs = re.findall(r'(-?\d+ \w+)', line)
+    pairs = [(int(n), c) for n, c in re.findall(r'(-?\d+) (\w+)', line)]
     mins = {'red':0, 'green':0, 'blue':0}
     valid = True
-    for pair in pairs:
-        num, color = int(pair.split()[0]), pair.split()[1]
+    for num, color in pairs:
         if num > MAXES[color]: valid = False
         mins[color] = max(num, mins[color])
     if valid: p1 += id
