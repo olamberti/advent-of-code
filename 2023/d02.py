37e406915bd6cec0d04b2f2df1fe2ed36c196a63
Oliver Lamberti <lamberti.oliver@gmail.com>
2023-12-02 07:01:46 +0100

    2023 solutions

--- 2023/d02.py
+++ 2023/d02.py
@@ -5,7 +5,7 @@ MAXES = {'red':12, 'green':13, 'blue':14}
 
 p1, p2 = 0, 0
 for id, line in enumerate(open('d02.txt').read().splitlines(), 1):
-    pairs = [(int(n), c) for n, c in re.findall(r'(-?\d+) (\w+)', line)]
+    pairs = [(int(n), c) for n, c in re.findall(r'(\d+) (\w+)', line)]
     mins = {'red':0, 'green':0, 'blue':0}
     valid = True
     for num, color in pairs:
