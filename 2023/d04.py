7d87803601f4b29f6ebdcad8798da37fd9e0e768
olamberti <olambert@volvocars.com>
2023-12-04 16:20:06 +0100

    2023 solutions

--- 2023/d04.py
+++ 2023/d04.py
@@ -4,10 +4,9 @@ numbers, p1, p2 = [1] * len(input), 0, 0
 for i, line in enumerate(input):
     wins, have = [set(x.split()) for x in line.split(': ')[1].split(' | ')]
     matches = len(wins & have)
-    for c in range(1, matches + 1):
-        if i + c < len(numbers): numbers[i + c] += numbers[i]
+    for n in range(matches):
+        numbers[i + 1 + n] += numbers[i]
     p1 += 2 ** (matches - 1) if matches > 0 else 0
     p2 += numbers[i]
 
-print(p1)
-print(p2)
+print(p1); print(p2)
