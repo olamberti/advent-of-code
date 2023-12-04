d9090276cff926b9ed8021430a8d40feec38c5af
olamberti <olambert@volvocars.com>
2023-12-04 16:34:49 +0100

    2023 solutions

--- 2023/d04.py
+++ 2023/d04.py
@@ -1,12 +1,12 @@
 input = open('d04.txt').read().splitlines()
-numbers, p1, p2 = [1] * len(input), 0, 0
+cards, p1 = [1] * len(input), 0
 
 for i, line in enumerate(input):
     wins, have = [set(x.split()) for x in line.split(': ')[1].split(' | ')]
     matches = len(wins & have)
     for n in range(matches):
-        numbers[i + 1 + n] += numbers[i]
+        cards[i + 1 + n] += cards[i]
     p1 += 2 ** (matches - 1) if matches > 0 else 0
-    p2 += numbers[i]
 
-print(p1); print(p2)
+print(p1)
+print(sum(cards))
