f0b7e8169ccd0187a88d159449c5ffc17e26eda4
Oliver Lamberti <lamberti.oliver@gmail.com>
2023-12-04 06:41:30 +0100

    2023 solutions

--- 2023/d04.py
+++ 2023/d04.py
@@ -1,12 +1,10 @@
-import re
-
 input = open('d04.txt').read().splitlines()
 numbers, p1, p2 = [1] * len(input), 0, 0
 
 for i, line in enumerate(input):
     wins, have = line.split(': ')[1].split('|')
-    wins = set([int(x) for x in re.findall(r'(\d+)', wins)])
-    have = set([int(x) for x in re.findall(r'(\d+)', have)])
+    wins = set([int(x) for x in wins.split()])
+    have = set([int(x) for x in have.split()])
     matches = len(wins & have)
     score = pow(2, matches - 1) if matches > 0 else 0
     for c in range(1, matches + 1):
