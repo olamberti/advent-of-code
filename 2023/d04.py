6fc4c8390f9a3506cfedb85c5f854a7985d9dee1
olamberti <olambert@volvocars.com>
2023-12-04 12:30:07 +0100

    2023 solutions

--- 2023/d04.py
+++ 2023/d04.py
@@ -2,12 +2,11 @@ input = open('d04.txt').read().splitlines()
 numbers, p1, p2 = [1] * len(input), 0, 0
 
 for i, line in enumerate(input):
-    wins, have = line.split(': ')[1].split(' | ')
-    matches = len(set(wins.split()) & set(have.split()))
-    score = pow(2, matches - 1) if matches > 0 else 0
+    wins, have = [set(x.split()) for x in line.split(': ')[1].split(' | ')]
+    matches = len(wins & have)
     for c in range(1, matches + 1):
         if i + c < len(numbers): numbers[i + c] += numbers[i]
-    p1 += score
+    p1 += 2 ** (matches - 1) if matches > 0 else 0
     p2 += numbers[i]
 
 print(p1)
