6759d02d7a01c1a9c34cd9632763a9df1667717d
olamberti <olambert@volvocars.com>
2023-12-05 13:01:51 +0100

    2023 solutions

--- 2023/d05.py
+++ 2023/d05.py
@@ -1,42 +1,40 @@
 import re
-input = open('d05.txt').read().split('\n\n')
 
-for i, line in enumerate(input):
-    if i == 0:
-        numbers, ranges = [int(x) for x in re.findall(r'(\d+)', line)], []
-        for n in range(len(numbers)//2):
-            ranges.append((numbers[2*n], numbers[2*n] + numbers[2*n+1] - 1))
-        continue
+data = open('d05.txt').read().split('\n\n')
+numbers = [int(x) for x in re.findall(r'(\d+)', data[0])]
+ranges = list(zip(numbers[0::2], [sum(x)-1 for x in zip(numbers[0::2], numbers[1::2])]))
 
-    rules, maps = [], line.splitlines()[1:]
-    for rule in maps:
-        rule = [int(x) for x in re.findall(r'(\d+)', rule)]
-        rules.append((rule[1], rule[1] + rule[2] - 1, rule[0] - rule[1]))
-    rules = sorted(rules)
+for i, line in enumerate(data[1:]):
+    # Input parsing
+    mappings = []
+    for mapping in line.splitlines()[1:]:
+        vals = [int(x) for x in re.findall(r'(\d+)', mapping)]
+        mappings.append((vals[1], vals[1] + vals[2] - 1, vals[0] - vals[1]))
+    mappings = sorted(mappings)
 
     # Part 1
-    for n in range(len(numbers)):
-        for low, high, diff in rules:
-            if low <= numbers[n] <= high:
-                numbers[n] += diff
+    for j, number in enumerate(numbers):
+        for low, high, diff in mappings:
+            if low <= number <= high:
+                numbers[j] += diff
                 break
     
     # Part 2
     cutted_ranges = []
     for n1, n2 in ranges:
         cuts = set()
-        for r1, r2, diff in rules:
+        for r1, r2, diff in mappings:
             if n1 <= r1 <= n2: cuts.add(r1)
             if n1 <= r2 <= n2: cuts.add(r2 + 1)
-        if cuts:
-            for c in sorted(list(cuts)):
-                if c == 0: continue
-                cutted_ranges.append((n1, c - 1))
-                n1 = c
+        for cut in sorted(list(cuts)):
+            if cut == 0: continue
+            cutted_ranges.append((n1, cut - 1))
+            n1 = cut
         cutted_ranges.append((n1, n2))
+    
     ranges, new_ranges = sorted(cutted_ranges), []   
     for n1, n2 in ranges:
-        for r1, r2, diff in rules:
+        for r1, r2, diff in mappings:
             if n2 < r1 or r2 < n1: continue
             else:
                 new_ranges.append((n1 + diff, n2 + diff))
