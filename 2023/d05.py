2861b8ef42b0287808cbb7a9921ec732e04a47bb
Oliver Lamberti <lamberti.oliver@gmail.com>
2023-12-05 20:41:25 +0100

    2023 solutions

--- 2023/d05.py
+++ 2023/d05.py
@@ -4,7 +4,7 @@ data = open('d05.txt').read().split('\n\n')
 numbers = [int(x) for x in re.findall(r'(\d+)', data[0])]
 ranges = list(zip(numbers[0::2], [sum(x)-1 for x in zip(numbers[0::2], numbers[1::2])]))
 
-for i, line in enumerate(data[1:]):
+for line in data[1:]:
     # Input parsing
     mappings = []
     for mapping in line.splitlines()[1:]:
@@ -13,33 +13,33 @@ for i, line in enumerate(data[1:]):
     mappings = sorted(mappings)
 
     # Part 1
-    for j, number in enumerate(numbers):
+    for i, number in enumerate(numbers):
         for low, high, diff in mappings:
             if low <= number <= high:
-                numbers[j] += diff
+                numbers[i] += diff
                 break
     
     # Part 2
     cutted_ranges = []
-    for n1, n2 in ranges:
+    for r1, r2 in ranges:
         cuts = set()
-        for r1, r2, diff in mappings:
-            if n1 <= r1 <= n2: cuts.add(r1)
-            if n1 <= r2 <= n2: cuts.add(r2 + 1)
+        for m1, m2, diff in mappings:
+            if r1 <= m1 <= r2: cuts.add(m1)
+            if r1 <= m2 <= r2: cuts.add(m2 + 1)
         for cut in sorted(list(cuts)):
             if cut == 0: continue
-            cutted_ranges.append((n1, cut - 1))
-            n1 = cut
-        cutted_ranges.append((n1, n2))
+            cutted_ranges.append((r1, cut - 1))
+            r1 = cut
+        cutted_ranges.append((r1, r2))
     
-    ranges, new_ranges = sorted(cutted_ranges), []   
-    for n1, n2 in ranges:
-        for r1, r2, diff in mappings:
-            if n2 < r1 or r2 < n1: continue
+    ranges, new_ranges = sorted(cutted_ranges), []
+    for r1, r2 in ranges:
+        for m1, m2, diff in mappings:
+            if r2 < m1 or m2 < r1: continue
             else:
-                new_ranges.append((n1 + diff, n2 + diff))
+                new_ranges.append((r1 + diff, r2 + diff))
                 break
-        else: new_ranges.append((n1, n2))
+        else: new_ranges.append((r1, r2))
     ranges = sorted(new_ranges)
 
 print(min(numbers))
