fa1d5bbc98479a5aa232746479a94b03ed614c3e
olamberti <olambert@volvocars.com>
2023-12-07 12:17:08 +0100

    2023 solutions

--- 2023/d07.py
+++ 2023/d07.py
@@ -1,46 +1,27 @@
-from collections import defaultdict
+def encode(hand, part2 = False):
+    labels, counts, value = 'X23456789TJQKA', {}, []
+    if part2: hand = hand.replace('J', 'X')
+    for card in labels:
+        counts[card] = hand.count(card)
+    jokers = counts.pop('X')
+    counts = sorted(counts.values(), reverse=True)[:2]
+    counts[0] += jokers
+    value.append([[1,1],[2,1],[2,2],[3,1],[3,2],[4,1],[5,0]].index(counts))
+    for card in hand:
+        value.append(labels.index(card))   
+    return tuple(value)
 
-labels = ['23456789TJQKA', 'J23456789TQKA']
-
-def encode(hand, p2 = False):
-    counts, id, js = defaultdict(int), [], 0
-    if not p2:
-        for card in hand:
-            counts[card] += 1
-            id.append(labels[0].index(card))
-        counts = sorted(counts.values(), reverse=True)
-    if p2:
-        for card in hand:
-            if card == 'J': js += 1
-            else:
-                counts[card] += 1
-            id.append(labels[1].index(card))
-        if js == 5: counts = [5, 0]
-        else:
-            counts = sorted(counts.values(), reverse=True)
-            counts[0] += js
-    if counts[0] == 5: id =  [7] + id
-    elif counts[0] == 4: id =  [6] + id
-    elif counts[0] == 3 and counts[1] == 2: id =  [5] + id
-    elif counts[0] == 3 and counts[1] == 1: id =  [4] + id
-    elif counts[0] == 2 and counts[1] == 2: id =  [3] + id
-    elif counts[0] == 2 and counts[1] == 1: id =  [2] + id
-    else: id =  [1] + id
-    return tuple(id)
-
-ranks_1, ranks_2, bids = [], [], {}
-for line in open('d07.txt').read().splitlines():
+ranks_1, ranks_2 = [], []
+for line in open('d07.txt'):
     hand, bid = line.split()
-    bids[hand] = int(bid)
-    ranks_1.append((encode(hand), hand))
-    ranks_2.append((encode(hand, True), hand))
-
+    ranks_1.append((encode(hand), int(bid)))
+    ranks_2.append((encode(hand, True), int(bid)))
 ranks_1 = sorted(ranks_1)
 ranks_2 = sorted(ranks_2)
 
 p1, p2 = 0, 0
 for i, item in enumerate(zip(ranks_1, ranks_2), 1):
-    p1 += i * bids[item[0][1]]
-    p2 += i * bids[item[1][1]] 
+    p1 += i * item[0][1]
+    p2 += i * item[1][1]
 print(p1)
 print(p2)
