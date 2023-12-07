82c729afe376b9f3bd6a86bbfd933f70316e1f21
Oliver Lamberti <lamberti.oliver@gmail.com>
2023-12-07 20:36:17 +0100

    2023 solutions

--- 2023/d07.py
+++ 2023/d07.py
@@ -1,4 +1,4 @@
-def encode(hand, part2 = False):
+def strength(hand, part2 = False):
     labels = 'X23456789TJQKA'
     if part2: hand = hand.replace('J', 'X')
     values = [labels.index(card) for  card in hand]
@@ -6,14 +6,14 @@ def encode(hand, part2 = False):
     jokers = counts.pop('X')
     counts = sorted(counts.values(), reverse=True)[:2]
     counts[0] += jokers
-    typ = [[[1,1],[2,1],[2,2],[3,1],[3,2],[4,1],[5,0]].index(counts)]
+    typ = ([1,1],[2,1],[2,2],[3,1],[3,2],[4,1],[5,0]).index(counts)
     return tuple([typ, *values])
 
 hands_1, hands_2 = [], []
 for line in open('d07.txt'):
     hand, bid = line.split()
-    hands_1.append((encode(hand), int(bid)))
-    hands_2.append((encode(hand, True), int(bid)))
+    hands_1.append((strength(hand), int(bid)))
+    hands_2.append((strength(hand, True), int(bid)))
 hands_1 = sorted(hands_1)
 hands_2 = sorted(hands_2)
 
