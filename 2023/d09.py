f4e2cf3689287e96f041269ce503921aed14f758
Oliver Lamberti <lamberti.oliver@gmail.com>
2023-12-09 19:22:31 +0100

    2023 solutions

--- 2023/d09.py
+++ 2023/d09.py
@@ -1,7 +1,6 @@
 def expol(x):
     if not any(x): return 0
-    dx = [j - i for i, j in zip(x, x[1:])]
-    return x[-1] + expol(dx)
+    return x[-1] + expol([j - i for i, j in zip(x, x[1:])])
 
 data = [[int(n) for n in line.split()] for line in open('d09.txt')]
 print(sum([expol(x) for x in data]))
