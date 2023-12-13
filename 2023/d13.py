1df1f4cc6c8aa47451c336348ad883973cf06ea8
olamberti <olambert@volvocars.com>
2023-12-13 12:13:56 +0100

    2023 solutions

--- 2023/d13.py
+++ 2023/d13.py
@@ -1,30 +1,18 @@
-def ref_val(grid, skip = -1):
+def ref_val(grid, mismatch = 0):
     for mul in (100, 1):
-        for x in range(len(grid) - 1):
-            if grid[x] == grid[x + 1]:
-                for i in range(min(x, len(grid) - 2 - x) + 1):
-                    if grid[x - i] != grid[x + 1 + i]: break
-                else:
-                    val = (x + 1) * mul
-                    if val != skip: return val
+        for x in range(1, len(grid)):
+            top = grid[:x][::-1]
+            bottom = grid[x:]
+            if sum(sum(e1 != e2 for e1, e2 in zip(tl, bl)) for tl, bl in zip(top, bottom)) == mismatch:
+                return mul * x
         grid = [list(line) for line in zip(*grid)]
     return 0
 
 p1, p2 = 0, 0
 for grid in open('d13.txt').read().split('\n\n'):
     grid = [list(line) for line in grid.splitlines()]
-    v1 = ref_val(grid)
-    p1 += v1
-    for r in range(len(grid)):
-        for c in range(len(grid[0])):
-            if grid[r][c] == '#': grid[r][c] = '.'
-            else: continue
-            v2 = ref_val(grid, v1)
-            grid[r][c] = '#'
-            if v2:
-                p2 += v2
-                break
-        if v2: break
+    p1 += ref_val(grid)
+    p2 += ref_val(grid, 1)
 
 print(p1)
 print(p2)
