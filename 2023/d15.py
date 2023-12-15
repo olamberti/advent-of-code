266a4c669204f7e7f520173c5a185906377b423f
Oliver Lamberti <lamberti.oliver@gmail.com>
2023-12-15 17:55:32 +0100

    2023 solutions

--- 2023/d15.py
+++ 2023/d15.py
@@ -9,19 +9,14 @@ seq = open('d15.txt').read().split(',')
 print(sum(hash(x) for x in seq))
 
 # Part 2
-boxes = [[] for _ in range(256)]
+boxes, lenses = [[] for _ in range(256)], {}
 for step in seq:
     label = step[:-1] if '-' in step else step[:-2]
     id = hash(label)
     if '=' in step:
-        foc = int(step[-1])
-        for i, lens in enumerate(boxes[id]):
-            if lens[0] == label:
-                boxes[id][i][1] = int(foc)
-                break
-        else: boxes[id].append([label, int(foc)])
+        lenses[label] = int(step[-1])
+        if label not in boxes[id]: boxes[id].append(label)
     else:
-        for i, lens in enumerate(boxes[id]):
-            if lens[0] == label: boxes[id].pop(i)
+        if label in boxes[id]: boxes[id].remove(label)
 
-print(sum(b * s * lens[1] for b, box in enumerate(boxes, 1) for s, lens in enumerate(box, 1)))
+print(sum(b * s * lenses[label] for b, box in enumerate(boxes, 1) for s, label in enumerate(box, 1)))
