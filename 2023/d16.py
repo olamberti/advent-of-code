a93b4ecaf2878ed7832ea4fc50451fad8f6d4c21
Oliver Lamberti <lamberti.oliver@gmail.com>
2023-12-16 07:04:01 +0100

    2023 solutions

---
+++ 2023/d16.py
@@ -0,0 +1,38 @@
+mappings = {'.': {1:[1], -1:[-1], 1j:[1j], -1j:[-1j]},
+            '|': {1:[1j, -1j], -1:[1j,-1j], 1j:[1j], -1j:[-1j]},
+            '-': {1:[1], -1:[-1], 1j:[1, -1], -1j:[1, -1]},
+            '\\':{1:[1j], -1:[-1j], 1j:[1], -1j:[-1]},
+            '/': {1:[-1j], -1:[1j], 1j:[-1], -1j:[1]}}
+
+grid = {}
+for y, line in enumerate(open("d16.txt")):
+    for x, ch in enumerate(line.strip()):
+        grid[x + y*1j] = ch
+width, height = x + 1, y + 1
+
+def energy(pos, d):
+    front, seen, lights = {(pos, d)}, set(), set()
+    while front:
+        new_front = set()
+        for pos, d in front:
+            if (pos, d) in seen: continue
+            if pos.real < 0 or pos.imag < 0 or pos.real >= width or pos.imag >= height: continue
+            seen.add((pos, d))
+            lights.add(pos)
+            for new_d in mappings[grid[pos]][d]:
+                new_front.add((pos + new_d, new_d))
+        front = new_front
+    return(len(lights))
+
+# Part 1
+print(energy(0, 1))
+
+# Part 2
+p2 = 0
+for x in range(width):
+    p2 = max(p2, energy(x, 1j))
+    p2 = max(p2, energy(x + height, -1j))
+for y in range(height):
+    p2 = max(p2, energy(y*1j, 1))
+    p2 = max(p2, energy(y*1j + width, -1))
+print(p2)
