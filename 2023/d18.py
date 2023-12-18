f08aebf7e7c85358b846f01d10212abe1dd0a7f6
Oliver Lamberti <lamberti.oliver@gmail.com>
2023-12-18 07:40:52 +0100

    2023 solutions

--- 2023/d18.py
+++ 2023/d18.py
@@ -6,25 +6,25 @@ v2, b2 = [(0, 0)], 0
 for line in open('d18.txt'):
     d, steps, code = line.split()
     # Part 1
+    x, y = v1[-1]
+    dx, dy = d1[d]
     steps = int(steps)
     b1 += steps
-    dx, dy = d1[d]
-    x, y = v1[-1]
     v1.append((x + dx * steps, y + dy * steps))
     # Part 2
     steps, d = int(code[2:-2], 16), code[-2]
-    b2 += steps
-    dx, dy = d2[d]
     x, y = v2[-1]
+    dx, dy = d2[d]
+    b2 += steps
     v2.append((x + dx * steps, y + dy * steps))
 
-def schoelace(edges): # Schoelace formula for area of a polygon
+def schoelace(edges): # Schoelace formula
     return abs(sum(edges[i][0] * (edges[(i+1)%len(edges)][1] - edges[(i-1)][1]) for i in range(len(edges)))) // 2
 
-def pick(area, b): # Pick's theorem (A = i + b/2 - 1)
+def pick(area, b): # Pick's theorem
     return area - b // 2 + 1
 
-def solve(edges, b): # Area = interior + boundary
+def solve(edges, b): # total area = interior + boundary
     return pick(schoelace(edges), b) + b
 
 print(solve(v1, b1))
