fe8c7a631ff5eee4b1016386284c66591e47e415
olamberti <olambert@volvocars.com>
2023-12-24 12:02:43 +0100

    2023 solutions

--- 2023/d24.py
+++ 2023/d24.py
@@ -13,14 +13,13 @@ for i, (px1, py1, _, vx1, vy1, _) in enumerate(hailstones):
         if a == b and c != d: continue # parallel and not equal lines
         x = (d - c) / (a - b)
         y = a * x + c
-        if low <= x <= high and low <= y <= high: # in range
-            if (vx1 * (x - px1) > 0 and vx2 * (x - px2) > 0): # positive time for both stones
-                total += 1
+        if low <= x <= high and low <= y <= high and vx1 * (x - px1) > 0 and vx2 * (x - px2) > 0:
+            total += 1 # both coordinates in range and positive time for both stones
 print(total)
 
 # Part 2 - Setting up a system of equations with the first three stones (9 equations, 9 unknowns)
 pxr, pyr, pzr, vxr, vyr, vzr, t0, t1, t2 = sympy.symbols('pxr pyr pzr vxr vyr vzr t0 t1 t2')
-equations, t = [], [t0, t1, t2]
+t, equations = [t0, t1, t2], []
 for i, (px, py, pz, vx, vy, vz) in enumerate(hailstones[:3]):
     equations.append(sympy.Eq(pxr + vxr * t[i], px + vx * t[i])) # x direction
     equations.append(sympy.Eq(pyr + vyr * t[i], py + vy * t[i])) # y direction
