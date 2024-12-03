1e41ec50b492b007108f0edebde913630f0c9d0b
olamberti <olambert@volvocars.com>
2024-12-03 15:58:30 +0100

    2024 solutions

--- 2024/d02.py
+++ 2024/d02.py
@@ -1,21 +1,19 @@
-def check(num):
-    if all(n in (1, 2, 3) for n in num) or all(n in (-1, -2, -3) for n in num):
-        return True
-    return False
+def is_safe(num):
+    return num <= {1, 2, 3} or num <= {-1, -2, -3}
 
 p1, p2= 0, 0
 
 for line in open('d02.txt').read().splitlines():
     nums = [int(x) for x in line.split()]
     diffs = set([nums[i] - nums[i-1] for i in range(1, len(nums))])
-    if check(diffs):
+    if is_safe(diffs):
         p1 += 1
         p2 += 1
         continue
     for i in range(len(nums)):
         new_nums = nums[:i] + nums[i+1:]
         diffs = set([new_nums[i] - new_nums[i-1] for i in range(1, len(new_nums))])
-        if check(diffs):
+        if is_safe(diffs):
             p2 += 1
             break
     
