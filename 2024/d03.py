eec8917d06d7d3d6ec95f83331ead75ccc2664a2
Oliver Lamberti <lamberti.oliver@gmail.com>
2024-12-03 06:57:57 +0100

    2024 solutions

---
+++ 2024/d03.py
@@ -0,0 +1,14 @@
+import re
+input = open('d03.txt').read()
+
+def add_muls(s):
+    muls = re.findall(r'mul\((\d+),(\d+)\)', s)
+    return sum([int(x) * int(y) for (x, y) in muls])
+   
+p1 = add_muls(input)
+print(p1)
+
+p2, input = 0, input.split('do()')
+for s in input:
+    p2 += add_muls(s.split('don' + "'" + 't')[0])
+print(p2)
