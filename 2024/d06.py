6ec61186724af41f91e0b060c025e7c9691350cd
olamberti <olambert@volvocars.com>
2024-12-06 11:27:32 +0100

    2024 solutions

--- 2024/d06.py
+++ 2024/d06.py
@@ -1,20 +1,39 @@
-walls, paths, dirs = set(), {}, {'>': 1, 'v': 1j, '<': -1,'^': -1j}
+walls, paths, cache, dirs = set(), set(), set(), {'>': 1, 'v': 1j, '<': -1,'^': -1j}
 
-for y, line in enumerate(open('d06t.txt').readlines()):
+for y, line in enumerate(open('d06.txt').readlines()):
     for x, e in enumerate(line.strip()):
         if e == '#': walls.add(x + y*1j)
         elif e in dirs.keys(): 
-            g_pos = x + y*1j
-            g_dir = dirs[e]
-H, W = y, x
+            pos = x + y*1j
+            dir = dirs[e]
+            paths.add(pos)
+            cache.add((pos, dir))
+W, H = x, y
 
-while 0 <= g_pos.real <= W and 0 <= g_pos.imag <= H:
-    if (g_pos + g_dir) not in walls:
-        paths[g_pos] = g_dir
-        g_pos += g_dir
+def in_bounds(p):
+    return 0 <= p.real <= W and 0 <= p.imag <= H
+
+obstacles = set()
+while in_bounds(pos):
+    if (pos + dir) in walls:
+        dir *= 1j
     else:
-        g_dir *= 1j
+        obs = pos + dir
+        if in_bounds(obs) and (obs) not in paths:
+            new_cache = cache.copy()
+            n_pos, n_dir= pos, dir * 1j
+            while in_bounds(n_pos):
+                if (n_pos + n_dir) in walls or (n_pos + n_dir) == obs:
+                    n_dir *= 1j
+                else:
+                    n_pos += n_dir
+                if (n_pos, n_dir) in new_cache:
+                    obstacles.add(obs)
+                    break
+                new_cache.add((n_pos, n_dir))
+        paths.add(pos)
+        cache.add((pos, dir))
+        pos += dir  
 
 print(len(paths))
-
-# part 2 missing, to be continued...
+print(len(obstacles))
