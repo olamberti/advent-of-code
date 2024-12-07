9c8bb1291d586cf12af34886c989fc5c8c22d545
Oliver Lamberti <lamberti.oliver@gmail.com>
2024-12-08 00:16:53 +0100

    2024 solutions

--- 2024/d06.py
+++ 2024/d06.py
@@ -6,22 +6,22 @@ for k, v in grid.items():
 
 paths, cache, obstacles = set(), set(), set()
 while pos in grid:
-    paths.add(pos), cache.add((pos, d))
+    paths.add(pos)
     if grid.get(pos + d) == '#':
         d *= 1j
     else:
         obs = pos + d
         if obs in grid and obs not in paths:
-            n_pos, n_d, n_cache = pos, d * 1j, cache.copy()
+            n_pos, n_d, cache = pos, d * 1j, set()
             while n_pos in grid:
-                n_cache.add((n_pos, n_d))
                 if grid.get(n_pos + n_d) == '#' or (n_pos + n_d) == obs:
+                    if (n_pos, n_d) in cache:
+                        obstacles.add(obs)
+                        break
+                    cache.add((n_pos, n_d))
                     n_d *= 1j
                 else:
                     n_pos += n_d
-                if (n_pos, n_d) in n_cache:
-                    obstacles.add(obs)
-                    break
         pos += d
 
 print(len(paths))
