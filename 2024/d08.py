d91f5b8f2f459aa8359f365d329518a01dfa9c19
Oliver Lamberti <lamberti.oliver@gmail.com>
2024-12-08 12:46:02 +0100

    2024 solutions

--- 2024/d08.py
+++ 2024/d08.py
@@ -1,29 +1,29 @@
-antennas = {}
+from collections import defaultdict
+
+antennas = defaultdict(list)
 for y, line in enumerate(open('d08.txt').readlines()):
     for x, e in enumerate(line.strip()):
         if e != '.':
-            if e not in antennas:
-                antennas[e] = [(x, y)]
-            else:
-                antennas[e].append((x, y))
+            antennas[e].append(x + y*1j)
 W, H = x, y
+
+def in_bounds(a):
+    return 0 <= a.real <= W and 0 <= a.imag <= H
 
 def calc_antinodes(antennas, p1 = True):
     antinodes = set()
-    for freq in antennas.values():
-        for i in range(len(freq)):
-            for j in range(i + 1, len(freq)):
-                a1, a2 = freq[i], freq[j]
-                dx, dy = a2[0] - a1[0], a2[1] - a1[1]
+    for pos in antennas.values():
+        for i in range(len(pos)):
+            for j in range(i + 1, len(pos)):
+                a1, a2, d = pos[i], pos[j], pos[j] - pos[i]
                 if not p1:
-                    antinodes.add(freq[i])
-                    antinodes.add(freq[j])  
-                while 0 <= a1[0] - dx <= W and 0 <= a1[1] - dy <= H:
-                    a1 = (a1[0] - dx, a1[1] - dy)
+                    antinodes.add(a1), antinodes.add(a2)
+                while in_bounds(a1 - d):
+                    a1 = a1 - d
                     antinodes.add(a1)
                     if p1: break
-                while 0 <= a2[0] + dx <= W and 0 <= a2[1] + dy <= H:
-                    a2 = (a2[0] + dx, a2[1] + dy)
+                while in_bounds(a2 + d):
+                    a2 = a2 + d
                     antinodes.add(a2)
                     if p1: break                  
     print(len(antinodes))
