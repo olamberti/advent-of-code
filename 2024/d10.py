baa3ad62742fb4f158226a1388e6e6aa599d04b7
olamberti <olambert@volvocars.com>
2024-12-10 13:14:04 +0100

    2024 solutions

--- 2024/d10.py
+++ 2024/d10.py
@@ -3,19 +3,17 @@ trailheads = [p for p, val in grid.items() if val == 0]
 
 p1, p2 = 0, 0
 for start in trailheads:
-    target, paths, ends = 1, {(start,)}, set()
+    target, paths = 1, {(start,)}
     while paths and target <= 9:
         new_paths = set()
         for path in paths:
             for d in [1, -1, 1j, -1j]:
                 new_pos = path[-1] + d
                 if new_pos in grid and grid[new_pos] == target:
-                        new_paths.add((*path, new_pos))
-                        if target == 9:
-                            ends.add(new_pos)
+                    new_paths.add((*path, new_pos))
         paths = new_paths
         target += 1
-    p1 += len(ends)
+    p1 += len(set([path[-1] for path in paths]))
     p2 += len(paths)
 
 print(p1)
