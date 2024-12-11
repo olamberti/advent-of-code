54c1333feed98203922f13a0fff27fe0a143c08b
olamberti <olambert@volvocars.com>
2024-12-11 10:44:32 +0100

    2024 solutions

--- 2024/d11.py
+++ 2024/d11.py
@@ -1,7 +1,7 @@
 input = list(map(int, open('d11.txt').read().split()))
 stones, cache = {x: input.count(x) for x in input}, {0: (1,)}
 
-def split(n):
+def blink(n):
     if n in cache:
         return cache[n]
     s = str(n)
@@ -12,15 +12,15 @@ def split(n):
     cache[n] = ans
     return ans
 
-for step in range(2000):
+for i in range(75):
     new_stones = {}
     for x, n in stones.items():
-        for y in split(x):
+        for y in blink(x):
             if y in new_stones:
                 new_stones[y] += n
             else:
                 new_stones[y] = n
-    if step == 25:
+    if i == 25:
         print(sum(stones.values()))
     stones = new_stones
           
