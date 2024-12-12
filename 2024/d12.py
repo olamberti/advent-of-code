6a49cdaafc70a808f581e2c5ce4c2d7125f16931
Oliver Lamberti <lamberti.oliver@gmail.com>
2024-12-12 21:47:25 +0100

    2024 solutions

--- 2024/d12.py
+++ 2024/d12.py
@@ -1,48 +1,43 @@
 grid = {x + y *1j: c for y, row in enumerate(open('d12.txt'))
                      for x, c in enumerate(row.strip())}
-
-p1, p2 = 0, 0
+p1, p2, dirs = 0, 0, [1, -1, 1j, -1j]
 while grid:
     pos = next(iter(grid.keys()))
     plant = grid.pop(pos)
 
-    front, region, edges = {pos}, {pos}, []
+    front, region, edges = {pos}, {pos}, {d: set() for d in dirs}
     while front:
         new_front = set()
         for pos in front:
-            for d in [1, -1, 1j, -1j]:
+            for d in dirs:
                 next_pos = pos + d
                 if next_pos in grid and grid[next_pos] == plant:
-                    new_front.add(next_pos)
-                    region.add(next_pos)
+                    new_front.add(next_pos), region.add(next_pos)
                     grid.pop(next_pos)
                 elif next_pos not in region:
-                    edges.append(set((pos + 0.5*(d + d*1j), pos + 0.5*(d - d*1j))))
+                    edges[d].add(next_pos)
         front = new_front
-    p1 += len(region) * len(edges)
+    p1 += len(region) * sum([len(x) for x in edges.values()])
 
     sides = 0
-    while edges:
-        a, b = edges.pop()
-        s, sv = b, a - b
-        while a != s:
-            v = a - b
-            next_pos = [edge for edge in edges if a in edge]
-            if len(next_pos) == 1:
-                c, d = next_pos[0]
-                edges.remove(next_pos[0])
-            else:
-                for i in range(len(next_pos)):
-                    c, d = next_pos[i]
-                    if not ((c - d) == v or (d - c) == v):
-                        edges.remove(next_pos[i])
-                        break
-            a, b = d if a == c else c, a
-            if v != a - b:
-                sides += 1
-            nv = a - b
-        if nv != sv:
+    for d, vals in edges.items():
+        seen = set()
+        for edge in vals:
+            if edge in seen:
+                continue
+            seen.add(edge)
             sides += 1
+            front = {edge}
+            while front:
+                new_front = set()
+                for pos in front:
+                    for next_pos in (pos + d * 1j, pos - d * 1j):
+                        if next_pos in seen:
+                            continue
+                        seen.add(next_pos)
+                        if next_pos in vals:
+                            new_front.add(next_pos)
+                front = new_front
     p2 += len(region) * sides
 
 print(p1)
