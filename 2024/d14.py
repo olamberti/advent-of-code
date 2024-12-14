4aed2bf994cdc2ce230e03e941e5c496fabca8c1
Oliver Lamberti <lamberti.oliver@gmail.com>
2024-12-14 21:59:42 +0100

    2024 solutions

--- 2024/d14.py
+++ 2024/d14.py
@@ -14,9 +14,6 @@ def safety_factor(ps):
     q4 = len([(x, y) for x, y in ps if x > W // 2 and y > H // 2])
     return q1 * q2 * q3 * q4
 
-def deviations(ps):
-    return (stdev([x for x, _ in ps]), stdev([y for _, y in ps]))
-
 def crt(a1, n1, a2, n2):
   if a1 < a2: a1, a2, n1, n2 = a2, a1, n2, n1
   q = [[a1, 1, 0], [a2, 0, 1]]
@@ -29,11 +26,11 @@ def crt(a1, n1, a2, n2):
   m1, m2 = q[-2][1], q[-2][2]
   return (n2 * m1 * a1 + n1 * m2 * a2) % (a1 * a2)
 
-minx, miny, rx, ry = float('inf'), float('inf'), None, None
+minx, miny = float('inf'), float('inf')
 for t in range(1, max(W, H) + 1):
     for i, (x, y) in enumerate(ps):
         ps[i] = ((x + vs[i][0]) % W, (y + vs[i][1]) % H)
-    stdx, stdy = deviations(ps)
+    stdx, stdy = stdev([x for x, _ in ps]), stdev([y for _, y in ps])
     if stdx < minx:
         minx, rx = stdx, t
     if stdy < miny:
