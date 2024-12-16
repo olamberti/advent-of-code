a3dbe3cda6b104f6d276419bf4a8ee37eb11952b
Oliver Lamberti <lamberti.oliver@gmail.com>
2024-12-16 22:50:58 +0100

    2024 solutions

--- 2024/d16.py
+++ 2024/d16.py
@@ -1,4 +1,4 @@
-import heapq
+from heapq import heappop, heappush
 
 grid = {(x, y): c for y, row in enumerate(open('d16.txt'))
                   for x, c in enumerate(row.strip())}
@@ -17,7 +17,7 @@ stack, cache = [(0, start, (1, 0), {start})], {}
 best_score, best_path = float('inf'), set()
 
 while stack:
-    score, pos, d, path = heapq.heappop(stack)
+    score, pos, d, path = heappop(stack)
     if score > best_score: break
     if (pos, d) in cache and cache[(pos, d)] < score: continue
     cache[pos, d] = score
@@ -26,13 +26,13 @@ while stack:
         best_path.update(path)
     for n_score, n_pos, n_d, n_path in next_steps(score, pos, d, path):
         for c_score, c_pos, c_d, c_path in stack:
-            if n_score == c_score and n_pos == c_pos and n_d == c_d:
+            if (c_score, c_pos, c_d) == (n_score, n_pos, n_d):
                 c_path.update(n_path)
                 break
             elif n_score > c_score:
-                heapq.heappush(stack, (n_score, n_pos, n_d, n_path))
+                heappush(stack, (n_score, n_pos, n_d, n_path))
                 break
-        else: heapq.heappush(stack, (n_score, n_pos, n_d, n_path))
+        else: heappush(stack, (n_score, n_pos, n_d, n_path))
         
 print(best_score)
 print(len(best_path))
