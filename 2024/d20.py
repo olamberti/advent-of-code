1a65715477fc3bbcb1c6cac6f2e6ebf5e20867a7
Oliver Lamberti <lamberti.oliver@gmail.com>
2024-12-20 08:16:19 +0100

    2024 solutions

--- 2024/d20.py
+++ 2024/d20.py
@@ -3,29 +3,30 @@ grid = {x + y *1j: c for y, row in enumerate(open('d20.txt'))
 start, = [pos for pos, c in grid.items() if c == 'S']
 goal,  = [pos for pos, c in grid.items() if c == 'E']
 
-pos, path = start, {start: 0}
+pos, paths = start, {start: 0}
 while pos != goal:
     for d in [1, -1, 1j, -1j]:
         next_pos = pos + d
-        if grid[next_pos] in '.E' and next_pos not in path:
-            path[next_pos] = path[pos] + 1
+        if grid[next_pos] in '.E' and next_pos not in paths:
+            paths[next_pos] = paths[pos] + 1
             pos = next_pos
 
 def dist(a, b):
     return int(abs(a.real - b.real) + abs(a.imag - b.imag))
 
-def count_best_cheats(time):
-    cheats = {}
-    for p1, d1 in path.items():
-        for p2, d2 in path.items():
-            if p1 == p2 or d2 - d1 <= 2: continue
-            if dist(p1, p2) <= time:
-                cheats[(p1, p2)] = d2 - d1 - dist(p1, p2)
-    ans, T = 0, 100
-    for x in cheats.values():
-        if x >= T: ans += 1
-    return ans
+def count_best_cheats():
+    path, c1, c2, T = list(paths.keys()), 0, 0, 100
+    for d1, p1 in enumerate(path):
+        for p2 in path[d1 + 3:]:
+            d = dist(p1, p2)
+            gain = paths[p2] - paths[p1] - d
+            if d <= 2 and gain >= T:
+                    c1 += 1
+            if d <= 20 and gain >= T:
+                    c2 += 1
+    return c1, c2
 
-print(count_best_cheats(2))
-print(count_best_cheats(20))
+p1, p2 = count_best_cheats()
+print(p1)
+print(p2)
 # Slow but works, might improve it later...
