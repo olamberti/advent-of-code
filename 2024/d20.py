21fbd4e8a0c0da469c1e4d629d2b0d7e304f2db5
olamberti <olambert@volvocars.com>
2024-12-21 00:51:27 +0100

    2024 solutions

--- 2024/d20.py
+++ 2024/d20.py
@@ -11,14 +11,27 @@ while pos != goal:
             dist[next_pos] = dist[pos] + 1
             pos = next_pos
 
-path, p1, p2, T = list(dist.keys()), 0, 0, 100
-for d1, a in enumerate(path):
-    for b in path[d1 + 3:]:
-        d = int(abs(a.real - b.real) + abs(a.imag - b.imag))
-        gain = dist[b] - dist[a] - d
-        if d <= 2 and gain >= T: p1 += 1
-        if d <= 20 and gain >= T: p2 += 1
+def get_within_dist(pos, distance):
+    front, seen, steps = {pos}, {pos: 0}, 0
+    while steps < distance:
+        steps += 1
+        new_front = set()
+        for pos in front:
+            for d in [1, -1, 1j, -1j]:
+                next_pos = pos + d
+                if next_pos not in seen:
+                    seen[next_pos] = steps
+                    new_front.add(next_pos)
+        front = new_front
+    return {x: d for x, d in seen.items() if x in dist and d > 1}
+
+p1, p2 = 0, 0
+for a in dist:
+    for b, d in get_within_dist(a, 20).items():
+        if dist[b] - dist[a] - d < 100: continue
+        if d <= 2: p1 += 1
+        p2 += 1
 
 print(p1)
 print(p2)
-# Slow but works, might improve it later...
+# Still slow but works, might improve it further later...
