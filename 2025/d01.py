e8d648d30f0bb72217237c0f19668635712a104b
Oliver Lamberti <oliver.lamberti@volvocars.com>
2025-12-01 08:58:11 +0100

    2025 solutions

--- 2025/d01.py
+++ 2025/d01.py
@@ -1,10 +1,18 @@
-pos, p1 = 50, 0
+pos, p1, p2 = 50, 0, 0
 
 for line in open('d01.txt').readlines():
     d, n = line[0], int(line[1:])
+    p2 += n // 100
+    n = n % 100
+    if d == 'R' and pos + n >= 100 and pos != 0:
+        p2 += 1
+    elif d == 'L' and pos - n <= 0 and pos != 0:
+        p2 += 1
     if d == 'L':
         n = (100 - n)
     pos = (pos + n) % 100
-    if pos == 0: p1 += 1
+    if pos == 0:
+        p1 += 1
 
 print(p1)
+print(p2)
