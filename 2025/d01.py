195d179a7c7f92231fd1115356d47c70fdbb4c9c
Oliver Lamberti <lamberti.oliver@gmail.com>
2025-12-01 21:31:24 +0100

    2025 solutions

--- 2025/d01.py
+++ 2025/d01.py
@@ -1,15 +1,13 @@
-pos, p1, p2 = 50, 0, 0
+pos, r, p1, p2 = 50, 'R', 0, 0              # position, direction, part1, part2
 
 for line in open('d01.txt').readlines():
-    d, n = line[0], int(line[1:])
-    if d == 'L':
-        pos = (100 - pos) % 100
-    p2 += (pos + n) // 100
-    pos = (pos + n) % 100
-    if pos == 0:
-        p1 += 1
-    if d == 'L':
-        pos = (100 - pos) % 100
+    nr, d = line[0], int(line[1:])          # new direction, distance
+    if nr!= r:                              # direction change
+        pos = (100 - pos) % 100             # mirror position (0 remains 0)
+        r = nr                              # update direction
+    p2 += (pos + d) // 100                  # part2: count reaching 100's
+    pos = (pos + d) % 100                   # update position
+    if pos == 0: p1 += 1                    # part1: count ending at 0
 
 print(p1)
 print(p2)
