04b74daf0c2bc778e859e0a35323ee0f0aac9282
Oliver Lamberti <lamberti.oliver@gmail.com>
2025-12-02 21:02:19 +0100

    2025 solutions

--- 2025/d02.py
+++ 2025/d02.py
@@ -1,30 +1,27 @@
-input, p1 = open('d02.txt').read(), 0
+input, p1, p2 = open('d02.txt').read(), 0, 0
 
 ns = [(int(a), int(b)) for a, b in (x.split('-') for x in input.split(','))]
 
+def patterns(a, b):
+    sa, sb, p1, p2 = str(a), str(b), set(), set()
+    if len(sa) != len(sb):
+        p11, p21 = patterns(a, int('9' * len(sa)))
+        p12, p22 = patterns(int('1' + '0' * len(sa)), b)
+        return p11 + p12, p21 + p22
+    for i in range(1, 10**(len(sa)//2)):
+        if len(sa) % len(str(i)) != 0:
+            continue
+        n = int(str(i) * (len(sa)//len(str(i))))
+        if a <= n <= b:
+            p2.add(n)
+            if len(sa)//len(str(i)) == 2:
+                p1.add(n)
+    return sum(p1), sum(p2)
+
 for a, b in ns:
-    sa, sb = str(a), str(b)
-    if len(sa) % 2 == 1 & len(sa) == len(sb):
-        continue
-    if len(sa) % 2 == 1:
-        smin = ('1' + (len(sa) - 1) // 2 * '0') * 2
-    else:
-        smin = sa
-    if len(sb) % 2 == 1:
-        smax = '9' * (len(sb) - 1)
-    else:
-        smax = sb
-
-    smin = [int(smin[:len(smin)//2]), int(smin[len(smin)//2:])]
-    smax = [int(smax[:len(smax)//2]), int(smax[len(smax)//2:])]
-
-    start = smin[0] if smin[0] >= smin[1] else smin[0] + 1
-    stop = smax[0] if smax[0] <= smax[1] else smax[0] - 1
-
-    if stop < start:
-        continue
-    for i in range(start, stop + 1):
-        n = int(str(i) * 2)
-        p1 += n
+    p11, p21 = patterns(a, b)
+    p1 += p11
+    p2 += p21
 
 print(p1)
+print(p2)
