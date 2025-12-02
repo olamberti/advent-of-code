8b7c00a41da66596cae8896a78f3a5ce1ab7baa9
Oliver Lamberti <lamberti.oliver@gmail.com>
2025-12-02 21:15:30 +0100

    2025 solutions

--- 2025/d02.py
+++ 2025/d02.py
@@ -1,27 +1,25 @@
-input, p1, p2 = open('d02.txt').read(), 0, 0
+input, p1, p2 = open('d02.txt').read(), 0, 0         # input, part1, part2   
+ns = [(int(a), int(b)) for a, b in                   # parsing input 
+      (x.split('-') for x in input.split(','))]
 
-ns = [(int(a), int(b)) for a, b in (x.split('-') for x in input.split(','))]
+def p(a, b):                                         # count patterns in [a, b]                  
+    sa, sb, p1, p2 = str(a), str(b), set(), set()    # strings, set for part1 & part2
+    if len(sa) != len(sb):                           # we split the range if lengths differ
+        p11, p21 = p(a, int('9' * len(sa)))          # first recursive part
+        p12, p22 = p(int('1' + '0' * len(sa)), b)    # second recursive part
+        return p11 + p12, p21 + p22                  # return combined results
+    for i in range(1, 10**(len(sa)//2)):             # generate pattern elements
+        if len(sa) % len(str(i)) != 0: continue      # skip if not fitting
+        n = int(str(i) * (len(sa)//len(str(i))))     # construct number from pattern
+        if a <= n <= b:                              # check if in range
+            p2.add(n)                                # part2: add to set
+            if len(sa)//len(str(i)) == 2: p1.add(n)  # part1: only patterns of half length              
+    return sum(p1), sum(p2)                          # return sums of set values
 
-def patterns(a, b):
-    sa, sb, p1, p2 = str(a), str(b), set(), set()
-    if len(sa) != len(sb):
-        p11, p21 = patterns(a, int('9' * len(sa)))
-        p12, p22 = patterns(int('1' + '0' * len(sa)), b)
-        return p11 + p12, p21 + p22
-    for i in range(1, 10**(len(sa)//2)):
-        if len(sa) % len(str(i)) != 0:
-            continue
-        n = int(str(i) * (len(sa)//len(str(i))))
-        if a <= n <= b:
-            p2.add(n)
-            if len(sa)//len(str(i)) == 2:
-                p1.add(n)
-    return sum(p1), sum(p2)
-
-for a, b in ns:
-    p11, p21 = patterns(a, b)
-    p1 += p11
-    p2 += p21
+for a, b in ns:                                      # process all ranges    
+    p11, p21 = p(a, b)                               # get patterns for range
+    p1 += p11                                        # accumulate part1              
+    p2 += p21                                        # accumulate part2
 
 print(p1)
 print(p2)
