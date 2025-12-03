3e8f3af3498637feca69c35be0a49bcfb911f0f2
Oliver Lamberti <lamberti.oliver@gmail.com>
2025-12-03 07:12:29 +0100

    2025 solutions

--- 2025/d03.py
+++ 2025/d03.py
@@ -3,8 +3,8 @@ p1, p2 = 0, 0                                           # part1, part2
 def jolt(n, b):                                         # max joltage with n digits from b
     if n == 1: return max(b)                            # base case
     n = n - 1                                           # reduce digit count                    
-    m = max(b[:-n])                                     # finding max in excluding last n digits                
-    return m * 10 ** n + jolt(n, b[b.index(m)+ 1:])     # build number and recurse
+    m = max(b[:-n])                                     # finding max excluding last n digits                
+    return m * 10 ** n + jolt(n, b[b.index(m)+1:])      # build number and recurse
 
 for line in open('d03.txt').readlines():                # read each input line
     bs = [int(x) for x in line.strip()]                 # parse as list of integers 
