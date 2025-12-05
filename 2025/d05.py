19da5af1fed9d00b420dc80009d46d684f69907e
Oliver Lamberti <oliver.lamberti@volvocars.com>
2025-12-05 16:35:27 +0100

    2025 solutions

--- 2025/d05.py
+++ 2025/d05.py
@@ -1,24 +1,19 @@
-ranges, ids = open('d05.txt').read().split('\n\n')
-ranges = [tuple(map(int, r.split('-'))) for r in ranges.split('\n')]
-ids = [int(i) for i in ids.split('\n')]
+rs, ids = open('d05.txt').read().split('\n\n')                 # read & split input
+rs = [tuple(map(int, r.split('-'))) for r in rs.split('\n')]   # parse ranges
+ids = [int(i) for i in ids.split('\n')]                        # parse ids
 
-p1 = 0
-for id in ids:
-    for r in ranges:
-        if r[0] <= id <= r[1]:
-            p1 += 1
-            break
+p1 = 0                                                         # part 1                                
+for id in ids:                                                 # loop through ids
+    for r1, r2 in rs:                                          # loop through ranges             
+        if r1 <= id <= r2:                                     # if id is in range                   
+            p1 += 1                                            # add to count  
+            break                                              # avoid double counting
 print(p1)
 
-merged = []
-ranges.sort()
-for r in ranges:
-    if not merged or merged[-1][1] < r[0]:
-        merged.append(r)
-    else:
-        merged[-1] = (merged[-1][0], max(merged[-1][1], r[1]))
-
-p2 = 0
-for m in merged:
-    p2 += m[1] - m[0] + 1
+p2, last = 0, 0                                                # part 2 & last number counted
+for r1, r2 in sorted(rs):                                      # loop through sorted ranges
+    start = max(r1, last + 1)                                  # determine new start   
+    if start <= r2:                                            # new numbers to count?
+        p2 += r2 - start + 1                                   # add to count   
+        last = r2                                              # update last number counted
 print(p2)
