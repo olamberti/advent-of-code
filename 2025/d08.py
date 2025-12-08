ec47a97adee1a17b25535bd13cf1dd3a987ca841
Oliver Lamberti <oliver.lamberti@volvocars.com>
2025-12-08 10:17:01 +0100

    2025 solutions

--- 2025/d08.py
+++ 2025/d08.py
@@ -1,32 +1,31 @@
-import math
+import heapq
 
 def dist(a, b):
-    return math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2 + (a[2] - b[2])**2)
+    return (a[0] - b[0])**2 + (a[1] - b[1])**2 + (a[2] - b[2])**2
 
-data = [tuple(map(int, line.split(','))) for line in open('d08.txt')]
+boxes = [tuple(map(int, line.split(','))) for line in open('d08.txt')]
+dists, circuits = [], []
+for i in range(len(boxes)):
+    circuits.append({boxes[i]})
+    for j in range(i + 1, len(boxes)):
+        heapq.heappush(dists, (dist(boxes[i], boxes[j]), (i, j)))
 
-ds, cs = dict(), []
-for i in range(len(data)):
-    cs.append({data[i]})
-    for j in range(i + 1, len(data)):
-        ds[dist(data[i], data[j])] = {data[i], data[j]}
-
-conn, n = sorted(ds.keys()), 0
-while len(cs) > 1:
-    j1, j2 = ds[conn[n]]
+n = 0
+while len(circuits) > 1:
     n += 1
-    for j, c in enumerate(cs):
-        if j1 in c:
+    _, (j1, j2) = heapq.heappop(dists)
+    for j, c in enumerate(circuits):
+        if boxes[j1] in c:
             i1 = j
             a = c
-        if j2 in c:
+        if boxes[j2] in c:
             i2 = j
-            b = c        
+            b = c       
     if n == 1000:
-        lens = sorted([len(c) for c in cs], reverse=True)
-        print(math.prod(lens[:3]))
+        lens = sorted([len(c) for c in circuits], reverse=True)
+        print(lens[0] * lens[1] * lens[2])
     if i1 == i2: continue
-    cs[i1] = a | b
-    del cs[i2]
+    circuits[i1] = a | b
+    del circuits[i2]
 
-print(j1[0] *  j2[0])
+print(boxes[j1][0] * boxes[j2][0])
