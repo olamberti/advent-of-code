b6dc83c240e0799da75be57d15968259a31a6541
Oliver Lamberti <lamberti.oliver@gmail.com>
2025-12-08 22:40:46 +0100

    2025 solutions

--- 2025/d08.py
+++ 2025/d08.py
@@ -3,25 +3,28 @@ import heapq
 def dist(a, b):
     return (a[0] - b[0])**2 + (a[1] - b[1])**2 + (a[2] - b[2])**2
 
-boxes = [tuple(map(int, line.split(','))) for line in open('d08.txt')]
+boxes = [tuple(map(int, row.split(','))) for row in open('d08.txt')]
 dists, circuits = [], []
 for i in range(len(boxes)):
     circuits.append({boxes[i]})
     for j in range(i + 1, len(boxes)):
-        heapq.heappush(dists, (dist(boxes[i], boxes[j]), (i, j)))
+        dists.append((dist(boxes[i], boxes[j]), (i, j)))
 
 n = 0
+heapq.heapify(dists)
 while len(circuits) > 1:
     n += 1
     _, (j1, j2) = heapq.heappop(dists)
+    i1, i2 = False, False
     for i, c in enumerate(circuits):
-        if boxes[j1] in c: i1, a = i, c
-        if boxes[j2] in c: i2, b = i, c      
+        if boxes[j1] in c: i1 = i
+        if boxes[j2] in c: i2 = i
+        if i1 and i2: break     
     if n == 1000:
         lens = sorted([len(c) for c in circuits], reverse=True)
         print(lens[0] * lens[1] * lens[2])
     if i1 == i2: continue
-    circuits[i1] |= b
+    circuits[i1] |= circuits[i2]
     del circuits[i2]
 
 print(boxes[j1][0] * boxes[j2][0])
