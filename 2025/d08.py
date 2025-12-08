3dcd9fce80419c3602400e5d0e3110385d0b6587
Oliver Lamberti <oliver.lamberti@volvocars.com>
2025-12-08 10:29:23 +0100

    2025 solutions

--- 2025/d08.py
+++ 2025/d08.py
@@ -15,17 +15,13 @@ while len(circuits) > 1:
     n += 1
     _, (j1, j2) = heapq.heappop(dists)
     for j, c in enumerate(circuits):
-        if boxes[j1] in c:
-            i1 = j
-            a = c
-        if boxes[j2] in c:
-            i2 = j
-            b = c       
+        if boxes[j1] in c: i1, a = j, c
+        if boxes[j2] in c: i2, b = j, c      
     if n == 1000:
         lens = sorted([len(c) for c in circuits], reverse=True)
         print(lens[0] * lens[1] * lens[2])
     if i1 == i2: continue
-    circuits[i1] = a | b
+    circuits[i1] |= b
     del circuits[i2]
 
 print(boxes[j1][0] * boxes[j2][0])
