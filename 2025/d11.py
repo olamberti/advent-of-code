7b8ed9ed58732548792226ee0615ec506cffbe04
Oliver Lamberti <oliver.lamberti@volvocars.com>
2025-12-11 13:04:58 +0100

    2025 solutions

--- 2025/d11.py
+++ 2025/d11.py
@@ -6,15 +6,11 @@ for row in open('d11.txt').read().splitlines():
     outs[start[:-1]] = ends
 
 @cache
-def n(pos, dest):
-    if pos == dest: return 1
-    if pos == 'out': return 0
-    return sum(n(out, dest) for out in outs[pos])
+def n(pos, fft, dac):
+    if pos == 'out': return 1 * fft * dac
+    elif pos == 'fft': fft = 1
+    elif pos == 'dac': dac = 1
+    return sum(n(out, fft, dac) for out in outs[pos])
 
-# Part 1
-print(n('you', 'out'))
-
-# Part 2:
-o1 = n('svr','fft') * n('fft','dac') * n('dac','out')
-o2 = n('svr','dac') * n('dac','fft') * n('fft','out')
-print(o1 + o2)
+print(n('you', 1, 1))
+print(n('svr', 0, 0))
