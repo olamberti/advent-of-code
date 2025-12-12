b7a0c555111f05937dba4b915e1eba58919947c8
Oliver Lamberti <oliver.lamberti@volvocars.com>
2025-12-12 17:28:03 +0100

    2025 solutions

--- 2025/d10.py
+++ 2025/d10.py
@@ -1,45 +1,43 @@
 from itertools import product
-from collections import deque as dq
+from functools import cache
 
 p1, p2 = 0, 0
 for row in open('d10.txt').read().splitlines():
     dia, *buttons, jolts = row.split()
-    dia = [1 if c == '#' else 0 for c in dia[1:-1]]
+    dia = tuple(1 if c == '#' else 0 for c in dia[1:-1])
     buttons = [tuple(map(int,x[1:-1].split(','))) for x in buttons]
-    jolts = [int(x) for x in jolts[1:-1].split(',')]
+    jolts = tuple([int(x) for x in jolts[1:-1].split(',')])
     nm, nb = len(dia), len(buttons)
 
-    # Part 1
-    pmin = nb
+    pmin, options = nb, {}
     for pressed in product((0, 1), repeat=nb):
-        lights = [0] * nm
+        js, n = [0] * nm, sum(pressed)
         for i, p in enumerate(pressed):
             if p == 0: continue
-            for id in buttons[i]: lights[id] = 1 - lights[id]
+            for id in buttons[i]: js[id] += 1
+        js = tuple(js)
+        if js not in options: options[js] = n
+        else: options[js] = min(options[js], n)
+        # Part 1 solution
+        lights = tuple(x % 2 for x in js)
         if lights == dia:
             pmin = min(sum(pressed), pmin)
     p1 += pmin
 
-    # Part 2
-    # BFS - Too slow for real input yet, only works for example...
-    q, cache = dq([[0] * nb]), set()
-    while q:
-        pressed = q.popleft()
-        if tuple(pressed) in cache: continue
-        cache.add(tuple(pressed))
+    @cache
+    def presses(target):
+        if all(x == 0 for x in target): return 0
+        if any(x < 0 for x in target): return float('inf')
 
-        js = [0] * nm
-        for i, p in enumerate(pressed):
-            if p == 0: continue
-            for id in buttons[i]: js[id] += p
-        if js == jolts:
-            p2 += sum(pressed)
-            break
-        else:
-            for i in range(nb):
-                new_pr = pressed[:]
-                new_pr[i] += 1
-                q.append(new_pr)
+        total = float('inf')
+        for diff, p in options.items():
+            new_target = tuple((b - a) for a, b in zip(diff, target))
+            if all(a % 2 == 0 for a in new_target):
+                half_target = tuple(x // 2 for x in new_target)
+                total = min(total, p + 2 * presses(half_target))
+        return total
+    
+    p2 += presses(jolts)
 
 print(p1)
 print(p2)
