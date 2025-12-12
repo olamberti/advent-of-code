3de11116b78fdfecbd413765b9fe5dcfa8a366d4
Oliver Lamberti <oliver.lamberti@volvocars.com>
2025-12-12 21:39:22 +0100

    2025 solutions

--- 2025/d10.py
+++ 2025/d10.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from itertools import product
 from functools import cache
 
@@ -7,32 +8,27 @@ for row in open('d10.txt').read().splitlines():
     dia = tuple(1 if c == '#' else 0 for c in dia[1:-1])
     buttons = [tuple(map(int,x[1:-1].split(','))) for x in buttons]
     jolts = tuple([int(x) for x in jolts[1:-1].split(',')])
-    nm, nb = len(dia), len(buttons)
 
-    pmin, options = nb, {}
-    for pressed in product((0, 1), repeat=nb):
-        js, n = [0] * nm, sum(pressed)
+    ops, patterns = {}, defaultdict(list)
+    for pressed in product((0, 1), repeat=len(buttons)):
+        jolt = [0] * len(jolts)
         for i, p in enumerate(pressed):
-            if p == 0: continue
-            for id in buttons[i]: js[id] += 1
-        js = tuple(js)
-        if js not in options: options[js] = n
-        else: options[js] = min(options[js], n)
-        # Part 1 solution
-        lights = tuple(x % 2 for x in js)
-        if lights == dia:
-            pmin = min(sum(pressed), pmin)
-    p1 += pmin
+            for j in buttons[i]: jolt[j] += p
+        lights = tuple(x % 2 for x in jolt)
+        ops[pressed] = jolt
+        patterns[lights] += [pressed]
+    p1 += min([sum(x) for x in patterns[dia]])
 
     @cache
     def presses(target):
         if all(x == 0 for x in target): return 0
         if any(x < 0 for x in target): return float('inf')
-        total = float('inf')
-        for diff, p in options.items():
-            new_target = tuple((b - a) for a, b in zip(diff, target))
-            if all(a % 2 == 0 for a in new_target):
-                total = min(total, p + 2 * presses(tuple(x // 2 for x in new_target)))
+        
+        total, lights = float('inf'), tuple(x % 2 for x in target)
+        for pressed in patterns[lights]:
+            diff = ops[pressed]
+            new_target = tuple((b - a) // 2 for a, b in zip(diff, target))
+            total = min(total, sum(pressed) + 2 * presses(new_target))
         return total 
     p2 += presses(jolts)
 
